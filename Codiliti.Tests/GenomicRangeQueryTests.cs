1c0216bc2c10ae0b16b556b97839e69566beb2ad
Illia Levandovskyi <lis2889@gmail.com>
2018-11-18 16:03:39 +0200

    GenomicRangeQuery solution - not verified

---
+++ Codiliti.Tests/GenomicRangeQueryTests.cs
@@ -0,0 +1,23 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class GenomicRangeQueryTests
+    {
+        [Fact]
+        public void MinImpactFactor_Sample_3()
+        {
+            var sequence = "CAGCCTA";
+            var starts = new[] { 2, 5, 0 };
+            var ends = new[] { 4, 5, 6 };
+
+            var solver = new GenomicRangeQuery();
+            var minImpactFactors = solver.MinImpactFactors(sequence, starts, ends).ToArray();
+
+            minImpactFactors.Should().Equal(new[] { 2, 4, 1 });
+        }
+    }
+}
---
+++ Codility.Solvers/GenomicRangeQuery.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codility.Solvers
+{
+    public class GenomicRangeQuery
+    {
+        private static readonly Dictionary<char, int> _impactFactors = new Dictionary<char, int>
+        {
+            ['A'] = 1,
+            ['C'] = 2,
+            ['G'] = 3,
+            ['T'] = 4,
+        };
+
+        public IEnumerable<int> MinImpactFactors(string nucleotids, int[] starts, int[] ends)
+        {
+            if (starts.Length != ends.Length)
+            {
+                throw new ArgumentException($"starts {starts.Length} should have the same count as ends {ends.Length}");
+            }
+
+            var nucleotidsArray = nucleotids.ToCharArray();
+
+            for (int pairIndex = 0; pairIndex < starts.Length; pairIndex++)
+            {
+                yield return GetMinimalImpactFactor(nucleotidsArray, starts[pairIndex], ends[pairIndex]);
+            }
+        }
+
+        private int GetMinimalImpactFactor(char[] nucleotids, int start, int end)
+        {
+            if (end > nucleotids.Length)
+                throw new Exception($"end {end} is to big - there are only {nucleotids.Length} symbols");
+
+            var minFactor = 4;
+            for (int i = start; i <= end; i++)
+            {
+                var currentFactor = _impactFactors[nucleotids[i]];
+                if (currentFactor < minFactor)
+                {
+                    if (currentFactor == 1)
+                        return 1;
+                    minFactor = currentFactor;
+                }
+            }
+
+            return minFactor;
+        }
+    }
+}
