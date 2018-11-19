9350efda15f9b91ce040da47dcb263358c333b67
ILya Lev <lis2889@gmail.com>
2018-11-19 17:14:50 +0100

    GenomicRangeQuery correct and fast solution

--- Codiliti.Tests/GenomicRangeQueryTests.cs
+++ Codiliti.Tests/GenomicRangeQueryTests.cs
@@ -14,10 +14,53 @@ namespace Codility.Tests
             var starts = new[] { 2, 5, 0 };
             var ends = new[] { 4, 5, 6 };
 
-            var solver = new GenomicRangeQuery();
-            var minImpactFactors = solver.MinImpactFactors(sequence, starts, ends).ToArray();
+            var solver = new GenomicRangeQuery(sequence);
+            var minImpactFactors = solver.MinImpactFactors(starts, ends).ToArray();
 
             minImpactFactors.Should().Equal(new[] { 2, 4, 1 });
+        }
+
+        [Fact]
+        public void MinImpactFactor_DoubleCharacterString_TwoPossibleValues()
+        {
+            var sequence = "TGTTGGGG";
+            var starts = new[] { 4, 5, 2 };
+            var ends = new[] { 7, 5, 3 };
+
+            var solver = new GenomicRangeQuery(sequence);
+            var minImpactFactors = solver.MinImpactFactors(starts, ends).ToArray();
+
+            minImpactFactors.Should().Equal(new[] { 3, 3, 4 });
+        }
+
+        [Fact]
+        public void MinImpactFactor_AllChars_Correct()
+        {
+                          //0123456789012345678901
+            var sequence = "ACTGTGCAACTGAGAGCCAATT";
+            var starts = new[] { 0, 5, 2, 7, 15, 20, 1 };
+            var ends = new[] { 21, 5, 3, 10, 17, 21, 6 };
+            var expectation = new[] { 1, 3, 3, 1, 2, 4, 2 };
+
+            var solver = new GenomicRangeQuery(sequence);
+            var minImpactFactors = solver.MinImpactFactors(starts, ends).ToArray();
+
+            minImpactFactors.Should().Equal(expectation);
+        }
+
+        [Fact]
+        public void MinImpactFactor_AlmostAllTheSame_Correct()
+        {
+                          //0123456789012345678901
+            var sequence = "GGGGGGGGGAGGGGGGGGGGGG";
+            var starts = new[] { 0, 0, 0, 9, 10, 20, 9 };
+            var ends = new[] { 21, 8, 9, 21, 21, 20, 9 };
+            var expectation = new[] { 1, 3, 1, 1, 3, 3, 1 };
+
+            var solver = new GenomicRangeQuery(sequence);
+            var minImpactFactors = solver.MinImpactFactors(starts, ends).ToArray();
+
+            minImpactFactors.Should().Equal(expectation);
         }
     }
 }
--- Codility.Solvers/GenomicRangeQuery.cs
+++ Codility.Solvers/GenomicRangeQuery.cs
@@ -13,39 +13,86 @@ namespace Codility.Solvers
             ['T'] = 4,
         };
 
-        public IEnumerable<int> MinImpactFactors(string nucleotids, int[] starts, int[] ends)
+        private readonly List<int>[] _impactIndexes;
+
+        public GenomicRangeQuery(string nucleotids)
+        {
+            var nucleotidsArray = nucleotids.ToCharArray();
+
+            _impactIndexes = new[]
+            {
+                new List<int>(nucleotidsArray.Length/4),
+                new List<int>(nucleotidsArray.Length/4),
+                new List<int>(nucleotidsArray.Length/4),
+                new List<int>(nucleotidsArray.Length/4)
+            };
+
+            for (int i = 0; i < nucleotidsArray.Length; i++)
+            {
+                var nucleotid = nucleotidsArray[i];
+                var impactFactor = _impactFactors[nucleotid];
+
+                _impactIndexes[impactFactor - 1].Add(i);
+            }
+        }
+
+        public IEnumerable<int> MinImpactFactors(int[] starts, int[] ends)
         {
             if (starts.Length != ends.Length)
             {
                 throw new ArgumentException($"starts {starts.Length} should have the same count as ends {ends.Length}");
             }
 
-            var nucleotidsArray = nucleotids.ToCharArray();
-
             for (int pairIndex = 0; pairIndex < starts.Length; pairIndex++)
             {
-                yield return GetMinimalImpactFactor(nucleotidsArray, starts[pairIndex], ends[pairIndex]);
+                yield return GetMinimalImpactFactor(starts[pairIndex], ends[pairIndex]);
             }
         }
 
-        private int GetMinimalImpactFactor(char[] nucleotids, int start, int end)
+        private int GetMinimalImpactFactor(int start, int end)
         {
-            if (end > nucleotids.Length)
-                throw new Exception($"end {end} is to big - there are only {nucleotids.Length} symbols");
-
-            var minFactor = 4;
-            for (int i = start; i <= end; i++)
+            for (int groupIndex = 0; groupIndex < _impactIndexes.Length; groupIndex++)
             {
-                var currentFactor = _impactFactors[nucleotids[i]];
-                if (currentFactor < minFactor)
-                {
-                    if (currentFactor == 1)
-                        return 1;
-                    minFactor = currentFactor;
-                }
+                var factorIndexes = _impactIndexes[groupIndex];
+                var firstGreaterOrEqual = GetFirstGreaterOrEqual(factorIndexes, start);
+
+                if (firstGreaterOrEqual != -1 && firstGreaterOrEqual <= end)
+                    return groupIndex + 1;
             }
 
-            return minFactor;
+            return 4;
+        }
+
+        private int GetFirstGreaterOrEqual(List<int> factorIndexes, int value)
+        {
+            if (factorIndexes.Count == 0)
+                return -1;
+
+            int start = 0, end = factorIndexes.Count;
+            int targetIndex = -1;
+            int middle = (start + end) / 2;
+            int previousMiddle = 0;
+            do
+            {
+                if (value == factorIndexes[middle])
+                    return value;
+
+                if (value > factorIndexes[middle])
+                {
+                    start = middle;
+                }
+                else if (value < factorIndexes[middle])
+                {
+                    targetIndex = middle;
+                    end = middle;
+                }
+
+                previousMiddle = middle;
+                middle = (start + end) / 2;
+
+            } while (middle < end && previousMiddle != middle);
+
+            return targetIndex >= 0 ? factorIndexes[targetIndex] : targetIndex;
         }
     }
 }
