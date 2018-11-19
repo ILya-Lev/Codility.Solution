2853890bb705e19cae062ed81500f279a6f4c41a
Illia Levandovskyi <lis2889@gmail.com>
2018-11-19 23:07:02 +0200

    lesson 6 distinct numbers - solution approved

---
+++ Codility.Solvers/Distinct.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Codility.Solvers
+{
+    public class Distinct
+    {
+        public int GetAmount(int[] numbers)             //n
+        {
+            var alreadySeen = new HashSet<int>();
+            foreach (var number in numbers)             //O(n)
+            {
+                if (!alreadySeen.Contains(number))      //O(1) as there is no collisions by def
+                    alreadySeen.Add(number);            //O(1) as there is no collisions by def
+            }
+
+            return alreadySeen.Count;                   //O(1)
+        }
+    }
+}
