8962f809cffb9133d76c250761958c0e7e9712be
Illia Levandovskyi <lis2889@gmail.com>
2018-11-20 00:43:45 +0200

    lesson 6 triangle - wrong solution - not approved

---
+++ Codiliti.Tests/TriangleTests.cs
@@ -0,0 +1,64 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class TriangleTests
+    {
+        [Fact]
+        public void ContainsTriangle_ContainsSample_True()
+        {
+            int[] sides = new[] { 10, 2, 5, 1, 8, 20 };
+            var solver = new Triangle();
+
+            var hasTriangle = solver.ContainsTriangle(sides);
+
+            hasTriangle.Should().BeTrue();
+        }
+        [Fact]
+        public void ContainsTriangle_AbsentSample_False()
+        {
+            int[] sides = new[] { 10, 50, 5, 1 };
+            var solver = new Triangle();
+
+            var hasTriangle = solver.ContainsTriangle(sides);
+
+            hasTriangle.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ContainsTriangle_AllTheSame_False()
+        {
+            int[] sides = new[] { 1, 1, 1, 1, 1, 1, 1 };
+            var solver = new Triangle();
+
+            var hasTriangle = solver.ContainsTriangle(sides);
+
+            hasTriangle.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ContainsTriangle_AllNegativeContains_True()
+        {
+            int[] sides = new[] { -10, -2, -5, -1, -8, -20 };
+            var solver = new Triangle();
+
+            var hasTriangle = solver.ContainsTriangle(sides);
+
+            hasTriangle.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ContainsTriangle_ArithmeticalProgression_True()
+        {
+            int[] sides = Enumerable.Range(-10, 20).ToArray();
+            var solver = new Triangle();
+
+            var hasTriangle = solver.ContainsTriangle(sides);
+
+            hasTriangle.Should().BeTrue();
+        }
+    }
+}
---
+++ Codility.Solvers/Triangle.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+
+namespace Codility.Solvers
+{
+    public class Triangle
+    {
+        public bool ContainsTriangle(int[] sides)
+        {
+            if (sides.Length < 3)
+                return false;
+
+            var sortedSides = sides.OrderBy(n => n).ToArray();
+            if (sortedSides.First() == sortedSides.Last())
+                return false;
+
+            var mean = sortedSides.Average();
+            var meanIndex = DefineIndex(sortedSides, mean);
+
+            var forCurrentMean = meanIndex - 1 >= 0
+                && meanIndex + 1 < sortedSides.Length
+                && sortedSides[meanIndex - 1] + sortedSides[meanIndex] > sortedSides[meanIndex + 1];
+
+            if (forCurrentMean) return true;
+
+            var forBiggerMean = meanIndex + 2 < sortedSides.Length
+                && sortedSides[meanIndex] + sortedSides[meanIndex + 1] > sortedSides[meanIndex + 2];
+
+            return forBiggerMean;
+        }
+
+        private int DefineIndex(int[] sortedSides, double mean)
+        {
+            var start = 0;
+            var end = sortedSides.Length;
+            while (start + 1 < end)
+            {
+                var middle = (start + end) / 2;
+                if ((int)mean == sortedSides[middle])
+                    return middle;
+
+                if (mean > sortedSides[middle])
+                {
+                    start = middle;
+                }
+                else
+                {
+                    end = middle;
+                }
+            }
+
+            return start;
+        }
+    }
+}
