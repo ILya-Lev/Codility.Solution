0c144233ec7e74e461397116edc6c0067e3411ce
Illia Levandovskyi <lis2889@gmail.com>
2018-11-21 00:58:25 +0200

    lesson 6 MaxProductOfThree - with tests - not as trivial as seems to be - approved

---
+++ Codiliti.Tests/MaxProductOfThreeTests.cs
@@ -0,0 +1,45 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class MaxProductOfThreeTests
+    {
+        [Fact]
+        public void GetMaxProduct_Sample_60()
+        {
+            int[] values = new[] { -3, 1, 2, -2, 5, 6 };
+            var solver = new MaxProductOfThree(values);
+            var product = solver.GetMaxProduct();
+            product.Should().Be(60);
+        }
+
+        [Fact]
+        public void GetMaxProduct_AllNegativeAnd0_0()
+        {
+            int[] values = new[] { -3, -1, -2, -2, -5, 0 };
+            var solver = new MaxProductOfThree(values);
+            var product = solver.GetMaxProduct();
+            product.Should().Be(0);
+        }
+
+        [Fact]
+        public void GetMaxProduct_BigNegativeNumbersAndSmallPositive_TwoNegativeAndOnePositive()
+        {
+            int[] values = new[] { -100, -50, 0, 1, 2, 3 };
+            var solver = new MaxProductOfThree(values);
+            var product = solver.GetMaxProduct();
+            product.Should().Be(100 * 50 * 3);
+        }
+
+        [Fact]
+        public void GetMaxProduct_ThreeBiggestGiveNegative_ComposePositiveProduct()
+        {
+            int[] values = new[] { -5, -4, 2, 3 };
+            var solver = new MaxProductOfThree(values);
+            var product = solver.GetMaxProduct();
+            product.Should().Be(60);
+        }
+    }
+}
---
+++ Codility.Solvers/MaxProductOfThree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility.Solvers
+{
+    public class MaxProductOfThree
+    {
+        private readonly int[] _values;
+
+        public MaxProductOfThree(int[] values)
+        {
+            if (values.Length < 3)
+            {
+                throw new Exception($"Input array should contain at least 3 items instead of {values.Length}");
+            }
+
+            _values = values;
+        }
+
+
+        public int GetMaxProduct()
+        {
+            var biggestNumbers = FindExtremalTriplet(_values, (a, b) => a < b);
+            var biggestNumbersProduct = biggestNumbers.Aggregate(1, (current, next) => current * next);
+
+            var smallestNumbers = FindExtremalTriplet(_values, (a, b) => a > b);
+            var combinedProduct = smallestNumbers[0] * smallestNumbers[1] * biggestNumbers[0];
+
+            return Math.Max(biggestNumbersProduct, combinedProduct);
+        }
+
+        private int[] FindExtremalTriplet(int[] values, Func<int, int, bool> relation)
+        {
+            var triplet = new int[3];
+            var storedIndexes = new HashSet<int>();
+            for (int tripletIndex = 0; tripletIndex < 3; tripletIndex++)
+            {
+                var maxIndex = 0;
+                for (int i = 0; i < values.Length; i++)
+                {
+                    if (!storedIndexes.Contains(i))
+                    {
+                        triplet[tripletIndex] = values[i];
+                        break;
+                    }
+                }
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    if (relation(triplet[tripletIndex], values[i]) && !storedIndexes.Contains(i))
+                    {
+                        triplet[tripletIndex] = values[i];
+                        maxIndex = i;
+                    }
+                }
+
+                storedIndexes.Add(maxIndex);
+            }
+
+            return triplet;
+        }
+
+    }
+}
