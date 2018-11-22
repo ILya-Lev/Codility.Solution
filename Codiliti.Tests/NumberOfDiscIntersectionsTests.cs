6ff13857d04d081a7c0440c43907c95af9d5365b
Illia Levandovskyi <lis2889@gmail.com>
2018-11-23 01:41:17 +0200

    lesson 6 NumberOfDiscIntersections - approved

---
+++ Codiliti.Tests/NumberOfDiscIntersectionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class NumberOfDiscIntersectionsTests
+    {
+        [Fact]
+        public void GetNumber_Sample_11()
+        {
+            var solver = new NumberOfDiscIntersections();
+            var radiuses = new[] { 1, 5, 2, 1, 4, 0 };
+            var intersections = solver.GetNumber(radiuses);
+            intersections.Should().Be(11);
+        }
+
+        [Fact]
+        public void GetNumber_SmallExtreme_2()
+        {
+            var solver = new NumberOfDiscIntersections();
+            var radiuses = new[] { 1, 2147483647, 0 };
+            var intersections = solver.GetNumber(radiuses);
+            intersections.Should().Be(2);
+        }
+    }
+}
---
+++ Codility.Solvers/NumberOfDiscIntersections.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+
+namespace Codility.Solvers
+{
+    public class NumberOfDiscIntersections
+    {
+        public int GetNumber(int[] radiuses)
+        {
+            var orderedRanges = ToOrderedRanges(radiuses);
+
+            var intersectionCounter = 0;
+            for (int i = 0; i < orderedRanges.Length; i++)
+            {
+                for (int j = i + 1; j < orderedRanges.Length && intersectionCounter <= 10_000_000; j++)
+                {
+                    if (orderedRanges[i].Intersects(orderedRanges[j]))
+                        intersectionCounter++;
+                    else
+                        break;
+                }
+            }
+
+            return intersectionCounter <= 10_000_000 ? intersectionCounter : -1;
+        }
+
+        private Range[] ToOrderedRanges(int[] radiuses)
+        {
+            return radiuses
+                .Select((r, n) => new Range
+                {
+                    Start = (long)n - r,
+                    End = (long)n + r
+                })
+                .OrderBy(r => r.Start)
+                .ThenByDescending(r => r.End)
+                .ToArray();
+        }
+    }
+
+    internal class Range
+    {
+        public long Start { get; set; }
+        public long End { get; set; }
+
+        public bool Intersects(Range other) //works only if other is smaller!
+        {
+            return Start <= other.Start && other.Start <= End
+                   || Start <= other.End && other.End <= End;
+        }
+    }
+}
