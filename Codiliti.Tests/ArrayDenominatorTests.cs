4e06b090481c979f19077a447e6ebe8bc113fe9c
ILya Lev <lis2889@gmail.com>
2018-11-27 18:59:43 +0100

    Lesson 8 Denominator - approved

---
+++ Codiliti.Tests/ArrayDenominatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class ArrayDenominatorTests
+    {
+        [Fact]
+        public void GetDenominatorIndex_Sample_0()
+        {
+            var values = new[] { 3, 4, 3, 2, 3, -1, 3, 3 };
+            var solver = new ArrayDenominator();
+
+            var index = solver.GetDenominatorIndex(values);
+
+            index.Should().Be(0);
+        }
+
+        [Fact]
+        public void GetDenominatorIndex_AllUnique_MinusOne()
+        {
+            var values = new[] { 7, 8, 9, 4, 5, 6, 1, 2, 3 };
+            var solver = new ArrayDenominator();
+
+            var index = solver.GetDenominatorIndex(values);
+
+            index.Should().Be(-1);
+        }
+
+        [Fact]
+        public void GetDenominatorIndex_Empty_MinusOne()
+        {
+            var values = new int[0];
+            var solver = new ArrayDenominator();
+
+            var index = solver.GetDenominatorIndex(values);
+
+            index.Should().Be(-1);
+        }
+    }
+}
---
+++ Codility.Solvers/ArrayDenominator.cs
@@ -0,0 +1,65 @@
+﻿using MoreLinq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility.Solvers
+{
+    public class ArrayDenominator
+    {
+        public int GetDenominatorIndex(int[] values)
+        {
+            if (values.Length == 0)
+            {
+                return -1;
+            }
+
+            var frequencies = CalculateFrequencies(values);
+            var mostFrequentValue = GetMostFrequentValue(frequencies);
+
+            if (mostFrequentValue.Value * 2 > values.Length)
+                return IndexOfValue(values, mostFrequentValue.Key);
+            return -1;
+        }
+
+        private static KeyValuePair<int,int> GetMostFrequentValue(Dictionary<int, int> frequencies)
+        {
+            var maxFrequence = frequencies.First();
+            foreach (var pair in frequencies.Skip(1))
+            {
+                if (maxFrequence.Value < pair.Value)
+                {
+                    maxFrequence = pair;
+                }
+            }
+
+            return maxFrequence;
+        }
+
+        private static Dictionary<int, int> CalculateFrequencies(int[] values)
+        {
+            var frequencies = new Dictionary<int, int>();
+            foreach (var value in values)
+            {
+                if (!frequencies.ContainsKey(value))
+                    frequencies[value] = 1;
+                else
+                    frequencies[value]++;
+            }
+
+            return frequencies;
+        }
+
+        private int IndexOfValue(int[] values, int value)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
