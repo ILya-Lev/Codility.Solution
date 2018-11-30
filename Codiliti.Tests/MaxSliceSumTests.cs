f081fa296d984cb4b165fc9b2fae27614f3d6908
Illia Levandovskyi <lis2889@gmail.com>
2018-11-30 21:47:22 +0200

    lesson 9 MaxSliceSum - slow but correct implementation

---
+++ Codiliti.Tests/MaxSliceSumTests.cs
@@ -0,0 +1,28 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class MaxSliceSumTests
+    {
+        [Fact]
+        public void GetMaxSliceSum_Sample_5()
+        {
+            var values = new[] { 3, 2, -6, 4, 0 };
+            var solver = new MaxSliceSum();
+            var maxSum = solver.GetMaxSliceSum(values);
+            maxSum.Should().Be(5);
+        }
+
+        [Fact]
+        public void GetMaxSliceSum_RepeatBig_SumAll()
+        {
+            var values = Enumerable.Repeat(1, 100_000).ToArray();
+            var solver = new MaxSliceSum();
+            var maxSum = solver.GetMaxSliceSum(values);
+            maxSum.Should().Be(100_000);
+        }
+    }
+}
---
+++ Codility.Solvers/MaxSliceSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Codility.Solvers
+{
+    public class MaxSliceSum
+    {
+        /// <summary>
+        /// O(n^2) implementation
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public int GetMaxSliceSum(int[] values)
+        {
+            long maxTotal = values[0];
+            for (int i = 0; i < values.Length; i++)
+            {
+                var total = 0L;
+                for (int j = i; j < values.Length; j++)
+                {
+                    total += values[j];
+                    if (total > maxTotal)
+                        maxTotal = total;
+                }
+            }
+
+            return Math.Abs(maxTotal) > int.MaxValue
+                ? maxTotal > 0 ? int.MaxValue : int.MinValue
+                : (int)maxTotal;
+        }
+    }
+}
