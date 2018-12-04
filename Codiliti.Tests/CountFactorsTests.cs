54923dda8264c10f75b2c75582a181a1e3820e5b
ILya Lev <lis2889@gmail.com>
2018-12-04 11:53:07 +0100

    Lesson 10 CountFactors - int.max is prime !

---
+++ Codiliti.Tests/CountFactorsTests.cs
@@ -0,0 +1,76 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Codility.Tests
+{
+    public class CountFactorsTests : IClassFixture<TestOutputHelper>
+    {
+        private readonly TestOutputHelper _outputHelper;
+
+        public CountFactorsTests(TestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(1)]
+        [Theory]
+        public void GetFactorsAmount_Small_1(int n)
+        {
+            var solver = new CountFactors();
+            var amount = solver.GetFactorsAmount(n);
+            amount.Should().Be(1);
+        }
+
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        [InlineData(11)]
+        [InlineData(13)]
+        [InlineData(17)]
+        [Theory]
+        public void GetFactorsAmount_Prime_2(int n)
+        {
+            var solver = new CountFactors();
+            var amount = solver.GetFactorsAmount(n);
+            amount.Should().Be(2);
+        }
+
+        [InlineData(4, 3)]
+        [InlineData(6, 4)]
+        [InlineData(8, 4)]
+        [InlineData(9, 3)]
+        [InlineData(10, 4)]
+        [InlineData(12, 6)]
+        [InlineData(14, 4)]
+        [Theory]
+        public void GetFactorsAmount_Composite_Many(int n, int expected)
+        {
+            var solver = new CountFactors();
+            var amount = solver.GetFactorsAmount(n);
+            amount.Should().Be(expected);
+        }
+
+        [Fact]
+        public void GetFactorsAmount_MaxInt_IsPrime()           //!!! 2^n-1 sometimes is prime; 2^32-1 indeed !
+        {
+            var solver = new CountFactors();
+            var amount = solver.GetFactorsAmount(int.MaxValue);
+            _outputHelper.WriteLine($"max int has {amount} divisors");
+        }
+
+        [Fact]
+        public void GetFactorsAmount_BigInt_Many()
+        {
+            var bigNumber = 2_147_395_600;
+            var solver = new CountFactors();
+            var amount = solver.GetFactorsAmount(bigNumber);
+            
+            _outputHelper.WriteLine($"{bigNumber} has {amount} divisors");
+        }
+    }
+}
---
+++ Codility.Solvers/CountFactors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Codility.Solvers
+{
+    public class CountFactors
+    {
+        public int GetFactorsAmount(int n)
+        {
+            if (n < 2) return 1;
+
+            var amount = 2;                                         //for 1 and n
+            
+            var maxDivisor = (int)Math.Ceiling(Math.Sqrt(n));       //lookup up to sqrt of n
+            if (maxDivisor == 1) return amount;
+
+            for (int i = 2; i < maxDivisor; i++)
+            {
+                if (n % i == 0)                              // if n divides by m, it divides by (n/m) as well
+                    amount += 2;
+            }
+
+            if (n == maxDivisor * maxDivisor)                // when n = k^2
+                amount++;
+
+            return amount;
+        }
+    }
+}
