d93135bbb7443cf32c964af439aab375fb89485c
ILya Lev <lis2889@gmail.com>
2018-12-04 12:15:31 +0100

    lesson 10 Min perimeter rectangle - approved

---
+++ Codiliti.Tests/MinPerimeterRectangleTests.cs
@@ -0,0 +1,46 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class MinPerimeterRectangleTests
+    {
+        [InlineData(1, 4)]
+        [InlineData(4, 8)]
+        [InlineData(9, 12)]
+        [Theory]
+        public void GetMinPerimeter_Square_4TimesRoot(int area, int perimeter)
+        {
+            var solver = new MinPerimeterRectangle();
+            var p = solver.GetMinPerimeter(area);
+            p.Should().Be(perimeter);
+        }
+
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        [InlineData(11)]
+        [Theory]
+        public void GetMinPerimeter_Prime_NPlus1Twice(int area)
+        {
+            var solver = new MinPerimeterRectangle();
+            var p = solver.GetMinPerimeter(area);
+            p.Should().Be(2 * (area + 1));
+        }
+
+        [InlineData(6, 10)]
+        [InlineData(8, 12)]
+        [InlineData(10, 14)]
+        [InlineData(12, 14)]
+        [InlineData(24, 20)]
+        [Theory]
+        public void GetMinPerimeter_Composite_ClosestToRoot(int area, int perimeter)
+        {
+            var solver = new MinPerimeterRectangle();
+            var p = solver.GetMinPerimeter(area);
+            p.Should().Be(perimeter);
+        }
+    }
+}
---
+++ Codility.Solvers/MinPerimeterRectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Codility.Solvers
+{
+    public class MinPerimeterRectangle
+    {
+        public int GetMinPerimeter(int area)
+        {
+            var maxDivider = (int) Math.Ceiling(Math.Sqrt(area));
+            for (int divider = maxDivider; divider >= 1; divider--)
+            {
+                if (area % divider == 0)
+                    return 2 * (divider + area / divider);
+            }
+
+            return -1;
+        }
+    }
+}
