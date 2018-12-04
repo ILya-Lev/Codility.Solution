8954bf14ada1c4f18f9e20c1b6f06d9d36d754fc
ILya Lev <lis2889@gmail.com>
2018-12-04 18:01:27 +0100

    lesson 10 Peaks - approved - estimated N*log(log(N)) analyze why

---
+++ Codiliti.Tests/PeaksTests.cs
@@ -0,0 +1,81 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class PeaksTests
+    {
+        [Fact]
+        public void GetBlockNumber_Sample_3()
+        {
+            var input = new[] { 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };
+            var solver = new Peaks();
+            var peaksNumber = solver.GetBlockNumber(input);
+            peaksNumber.Should().Be(3);
+        }
+
+        [Fact]
+        public void GetBlockNumber_PrimeLength_1()
+        {
+            var input = new[] { 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6 };
+            var solver = new Peaks();
+            var peaksNumber = solver.GetBlockNumber(input);
+            peaksNumber.Should().Be(1);
+        }
+
+        [Fact]
+        public void GetBlockNumber_Empty_0()
+        {
+            var input = new int[0];
+            var solver = new Peaks();
+            var peaksNumber = solver.GetBlockNumber(input);
+            peaksNumber.Should().Be(0);
+        }
+        
+        //they say item could be peak if has 2 neighbors
+        [Fact]
+        public void GetBlockNumber_OneItem_0()
+        {
+            var input = new[] { 1};
+            var solver = new Peaks();
+            var peaksNumber = solver.GetBlockNumber(input);
+            peaksNumber.Should().Be(0);
+        }
+
+        [InlineData(2)]
+        [InlineData(10)]
+        [InlineData(11)]
+        [InlineData(12)]
+        [Theory]
+        public void GetBlockNumber_AllItemsTheSame_0(int arrayLength)
+        {
+            var input = Enumerable.Repeat(1, arrayLength).ToArray();
+            var solver = new Peaks();
+            var peaksNumber = solver.GetBlockNumber(input);
+            peaksNumber.Should().Be(0);
+        }
+
+        [Fact]
+        public void GetBlockNumber_BigDecreaseIncrease_2()
+        {
+            int size = 55_000;
+            var input = Enumerable.Range(1, size).Reverse().Concat(Enumerable.Range(1,size)).ToArray();
+            Swap(input, 0, 1);
+            Swap(input, input.Length - 1, input.Length - 2);
+            var solver = new Peaks();
+
+            var peaksNumber = solver.GetBlockNumber(input);
+
+            peaksNumber.Should().Be(2);
+        }
+
+        private void Swap(int[] input, int lhs, int rhs)
+        {
+            var tmp = input[lhs];
+            input[lhs] = input[rhs];
+            input[rhs] = tmp;
+        }
+    }
+}
---
+++ Codility.Solvers/Peaks.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility.Solvers
+{
+    public class Peaks
+    {
+        public int GetBlockNumber(int[] array)      // estimated to have complexity N * log(log(N)) ... why?
+        {
+            if (array.Length == 0)
+                return 0;
+            if (array.Length == 1)
+                return 0;
+
+            var peakIndices = FindAllPeaks(array);
+            if (peakIndices.Count == 0)             //there is no peaks - even one block solution does not fit
+                return 0;
+
+            var arrayLengthDividers = FindDividers(array.Length).Distinct().OrderBy(d => d).ToArray();
+
+            foreach (var blockLength in arrayLengthDividers)
+            {
+                var blockAmount = array.Length / blockLength;
+                if (EachBlockHasPeak(peakIndices, blockLength, blockAmount))
+                {
+                    return blockAmount;
+                }
+            }
+
+            return 0;
+        }
+
+        private HashSet<int> FindAllPeaks(int[] array)
+        {
+            var peakIndices = new HashSet<int>();
+            //if (array[0] > array[1])
+            //    peakIndices.Add(0);
+            //if (array[array.Length - 1] > array[array.Length - 2])
+            //    peakIndices.Add(array.Length - 1);
+
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i - 1] && array[i] > array[i + 1])
+                {
+                    peakIndices.Add(i);
+                }
+            }
+
+            return peakIndices;
+        }
+
+        private IEnumerable<int> FindDividers(int n)
+        {
+            yield return 1;
+            yield return n;
+
+            var maxDivider = (int)Math.Ceiling(Math.Sqrt(n));
+            for (int divider = 2; divider <= maxDivider; divider++)
+            {
+                if (n % divider == 0)
+                {
+                    yield return divider;
+                    yield return n / divider;
+                }
+            }
+        }
+
+        private bool EachBlockHasPeak(HashSet<int> peakIndices, int blockLength, int blockAmount)
+        {
+            for (int i = 0; i < blockAmount; i++)
+            {
+                var block = Enumerable.Range(i * blockLength, blockLength).ToArray();
+                if (!BlockHasPeak(peakIndices, block))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool BlockHasPeak(HashSet<int> peakIndices, int[] block)
+        {
+            return block.Any(peakIndices.Contains);
+        }
+
+    }
+}
