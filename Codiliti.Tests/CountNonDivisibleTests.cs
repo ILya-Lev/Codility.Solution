d797c7dc3a7fc0e68f894be647532f7602b47f1a
ILya Lev <lis2889@gmail.com>
2018-12-05 11:22:47 +0100

    lesson 11 - CountNonDivisible - on 20k items ten times slower then expected; try factorization approach

---
+++ Codiliti.Tests/CountNonDivisibleTests.cs
@@ -0,0 +1,47 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class CountNonDivisibleTests
+    {
+        [Fact]
+        public void GetNonDivisibleAmount_Sample_Sample()
+        {
+            var values = new[] { 3, 1, 2, 3, 6 };
+            var solver = new CountNonDivisible();
+            var sequence = solver.GetNonDivisibleAmount(values);
+            sequence.Should().Equal(new[] {2, 4, 3, 2, 0});
+        }
+
+        [Fact]
+        public void GetNonDivisibleAmount_OneToTen_Fast()
+        {
+            var values = Enumerable.Range(1, 10).ToArray();
+            var solver = new CountNonDivisible();
+            var sequence = solver.GetNonDivisibleAmount(values);
+            sequence.Should().Equal(new[] { 9, 8, 8, 7, 8, 6, 8, 6, 7, 6 });
+        }
+
+        [Fact]
+        public void GetNonDivisibleAmount_TenToOne_Fast()
+        {
+            var values = Enumerable.Range(1, 10).Reverse().ToArray();
+            var solver = new CountNonDivisible();
+            var sequence = solver.GetNonDivisibleAmount(values);
+            sequence.Should().Equal(new[] { 9, 8, 8, 7, 8, 6, 8, 6, 7, 6 }.Reverse());
+        }
+
+        [Fact]
+        public void GetNonDivisibleAmount_AllTheSame_Zeros()
+        {
+            var size = 10_000;
+            var values = Enumerable.Repeat(1,size).ToArray();
+            var solver = new CountNonDivisible();
+            var sequence = solver.GetNonDivisibleAmount(values);
+            sequence.Should().Equal(Enumerable.Repeat(0, size));
+        }
+    }
+}
---
+++ Codility.Solvers/CountNonDivisible.cs
@@ -0,0 +1,62 @@
+﻿using MoreLinq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility.Solvers
+{
+    public class CountNonDivisible
+    {
+        public int[] GetNonDivisibleAmount(int[] values)
+        {
+            //O(N)
+            var frequencyTable = FillFrequencyTable(values);
+            //O(N*log(N))
+            var uniqueIncreasingValues = frequencyTable.Keys.Cast<int>().OrderBy(k => k).ToArray();
+            //O(N^2)
+            var counterByUniqueValue = GetAmountByUniqueSortedValue(values, uniqueIncreasingValues, frequencyTable);
+            //O(N)
+            return values.Select(v => counterByUniqueValue[v]).ToArray();
+        }
+
+        private Dictionary<int,int> FillFrequencyTable(int[] input)
+        {
+            var frequency = new Dictionary<int, int>();
+
+            foreach (var number in input)
+            {
+                if (!frequency.ContainsKey(number))
+                    frequency.Add(number, 1);
+                else
+                    frequency[number]++;
+            }
+
+            return frequency;
+        }
+
+        private Dictionary<int, int> GetAmountByUniqueSortedValue(int[] values, int[] uniqueIncreasingValues, Dictionary<int, int> frequencyTable)
+        {
+            var counterByUniqueValue = new Dictionary<int, int>();
+            for (int i = 0; i < uniqueIncreasingValues.Length; ++i)
+            {
+                var nonDividableCounter = values.Length - CalculateDividable(uniqueIncreasingValues, i, frequencyTable);
+                counterByUniqueValue.Add(uniqueIncreasingValues[i], nonDividableCounter);
+            }
+
+            return counterByUniqueValue;
+        }
+
+        private int CalculateDividable(int[] sortedUnique, int currentIndex, Dictionary<int, int> frequencyTable)
+        {
+            var dividable = 0;
+            for (int i = 0; i <= currentIndex; i++)         //the number is dividable by itself
+            {
+                if (sortedUnique[currentIndex] % sortedUnique[i] == 0)
+                {
+                    dividable += frequencyTable[sortedUnique[i]];
+                }
+            }
+
+            return dividable;
+        }
+    }
+}
