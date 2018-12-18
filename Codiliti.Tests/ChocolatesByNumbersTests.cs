393428b25445caf36dee3192dea55e5b93e8d9c2
Illia Levandovskyi <lis2889@gmail.com>
2018-12-19 01:05:08 +0200

    lesson 12 chocolates by numbers - smallest common divisible divide by second number - approved

---
+++ Codiliti.Tests/ChocolatesByNumbersTests.cs
@@ -0,0 +1,57 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class ChocolatesByNumbersTests
+    {
+        [Fact]
+        public void OverallEaten_Sample_5()
+        {
+            var solver = new ChocolatesByNumbers();
+            var amount = solver.OverallEaten(10, 4);
+            amount.Should().Be(5);
+        }
+
+        [Fact]
+        public void OverallEaten_AllDividableByStep_DivisionTime()
+        {
+            var solver = new ChocolatesByNumbers();
+            var amount = solver.OverallEaten(10, 5);
+            amount.Should().Be(2);
+        }
+
+        [Fact]
+        public void OverallEaten_AllEqualToStep_1()
+        {
+            var solver = new ChocolatesByNumbers();
+            var amount = solver.OverallEaten(10, 10);
+            amount.Should().Be(1);
+        }
+
+        [Fact]
+        public void OverallEaten_AllSemiprimeToStep_All()
+        {
+            var solver = new ChocolatesByNumbers();
+            var amount = solver.OverallEaten(10, 3);
+            amount.Should().Be(10);
+        }
+
+        [Fact]
+        public void OverallEaten_AllSmallerThanStep_UntilValueRepeats()
+        {
+            var solver = new ChocolatesByNumbers();
+            var amount = solver.OverallEaten(4, 10);
+            amount.Should().Be(2);
+        }
+
+        [Fact]
+        public void OverallEaten_AllSemiprimeStep_UntilValueRepeats()
+        {
+            var solver = new ChocolatesByNumbers();
+            var amount = solver.OverallEaten(947853, 4453);
+            amount.Should().Be(947853);
+        }
+    }
+}
---
+++ Codility.Solvers/ChocolatesByNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Codility.Solvers
+{
+    public class ChocolatesByNumbers
+    {
+        public int OverallEaten(int all, int step)
+        {
+            //the answer is smallest common divisable (scd) divided by smaller number
+            // scd is a*b / largest common factor (lcf)
+            // lcf we will find by euclidian algorithm
+
+            var lcf = CalculateLCF(all, step);
+
+            var scd = (long)all * step / lcf;
+            return (int)(scd / step);
+
+        }
+
+        private int CalculateLCF(int a, int b)
+        {
+            var bigger = Math.Max(a, b);
+            var smaller = Math.Min(a, b);
+            if (bigger % smaller == 0)
+                return smaller;
+            return CalculateLCF(bigger % smaller, smaller);
+        }
+    }
+}
