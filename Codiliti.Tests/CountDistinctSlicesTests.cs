5d3f0633522c83c6ee93cabdd9cd94fcf99a4a4c
Illia Levandovskyi <lis2889@gmail.com>
2019-02-08 00:13:27 +0100

    lesson 15 CountDistinctSlices - wrong solution as does not handle repeating elements properly

---
+++ Codiliti.Tests/CountDistinctSlicesTests.cs
@@ -0,0 +1,85 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class CountDistinctSlicesTests
+    {
+        [Fact]
+        public void CalculateDistinctSlices_Sample_9()
+        {
+            var input = new int[] { 3, 4, 5, 5, 2 };
+            var number = new CountDistinctSlices().CalculateDistinctSlices(input);
+            number.Should().Be(9);
+        }
+
+        [Fact]
+        public void CalculateDistinctSlices_Empty_0()
+        {
+            var input = new int[0];
+            var number = new CountDistinctSlices().CalculateDistinctSlices(input);
+            number.Should().Be(0);
+        }
+
+        [Fact]
+        public void CalculateDistinctSlices_AllTheSame_LengthOfTheInput()
+        {
+            var input = new int[] { 1, 1, 1, 1 };
+            var number = new CountDistinctSlices().CalculateDistinctSlices(input);
+            number.Should().Be(input.Length);
+        }
+
+        [Fact]
+        public void CalculateDistinctSlices_AllDifferent_ArithmeticSum()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5 };
+            var number = new CountDistinctSlices().CalculateDistinctSlices(input);
+            number.Should().Be(input.Length * (input.Length + 1) / 2);
+        }
+
+        [Fact]
+        public void CalculateDistinctSlices_One_One()
+        {
+            var input = new int[] { 1 };
+            var number = new CountDistinctSlices().CalculateDistinctSlices(input);
+            number.Should().Be(input.Length * (input.Length + 1) / 2);
+        }
+
+        [Fact]
+        public void CalculateDistinctSlices_TwoIdentical_2()
+        {
+            var input = new int[] { 1, 1 };
+            var number = new CountDistinctSlices().CalculateDistinctSlices(input);
+            number.Should().Be(2);
+        }
+
+        [Fact]
+        public void CalculateDistinctSlices_101_5()
+        {
+            var input = new int[] { 1, 0, 1 };
+            var number = new CountDistinctSlices().CalculateDistinctSlices(input);
+            number.Should().Be(5);
+        }
+
+        [Fact]
+        public void CalculateDistinctSlices_WithMaxMin_ArithmeticSum()
+        {
+            var input = new int[] { int.MaxValue, int.MinValue, 0, 3 };
+            var number = new CountDistinctSlices().CalculateDistinctSlices(input);
+            number.Should().Be(input.Length * (input.Length + 1) / 2);
+        }
+
+        [Fact]
+        public void CalculateDistinctSlices_Random_SomeResult()
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+            var input = Enumerable.Range(1, 20).Select(n => random.Next(1, 10)).ToArray();
+            var number = new CountDistinctSlices().CalculateDistinctSlices(input);
+            number.Should().BeGreaterThan(30);
+        }
+
+    }
+}
---
+++ Codility.Solvers/CountDistinctSlices.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Codility.Solvers
+{
+    public class CountDistinctSlices
+    {
+        public int CalculateDistinctSlices(int[] sequence)
+        {
+            if (sequence.Length == 0)
+                return 0;
+
+            var totalSlices = 0;
+            var slice = new HashSet<int>();
+            slice.Add(sequence[0]);
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                if (slice.Contains(sequence[i]))
+                {
+                    totalSlices += GetNumberOfSubSlices(slice.Count);
+                    slice.Clear();
+                    slice.Add(sequence[i - 1]);
+                }
+                slice.Add(sequence[i]);
+            }
+            totalSlices += GetNumberOfSubSlices(slice.Count);
+
+            return totalSlices;
+        }
+
+        private int GetNumberOfSubSlices(int sequenceLength)
+        {
+            return sequenceLength * (sequenceLength + 1) / 2;
+        }
+    }
+}
