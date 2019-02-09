6b6504613ad6da5ed6cc5dd0555c61e992ba716e
Illia Levandovskyi <lis2889@gmail.com>
2019-02-09 01:56:32 +0100

    lesson 15 CountTriangles - 45%

---
+++ Codiliti.Tests/CountTrianglesTests.cs
@@ -0,0 +1,43 @@
+﻿using Codility.Solvers;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class CountTrianglesTests
+    {
+        [Fact]
+        public void CountTriplets_Sample_4()
+        {
+            var values = new[] { 10, 2, 5, 1, 8, 12 };
+            var tripletsNumber = new CountTriangles().CountTriplets(values);
+            tripletsNumber.Should().Be(4);
+        }
+
+        [Fact]
+        public void CountTriplets_Empty_0()
+        {
+            var values = new int[0];
+            var tripletsNumber = new CountTriangles().CountTriplets(values);
+            tripletsNumber.Should().Be(0);
+        }
+
+        [Fact]
+        public void CountTriplets_TheSame_Cn3()
+        {
+            var values = new[] { 1, 1, 1, 1, 1, 1 };
+            var tripletsNumber = new CountTriangles().CountTriplets(values);
+            tripletsNumber.Should().Be(20); //as C from 6 by 3 = 6!/3!/(6-3)!
+        }
+
+        [Fact]
+        public void CountTriplets_1To9_34()
+        {
+            var values = Enumerable.Range(1, 9).Reverse().ToArray();
+            var tripletsNumber = new CountTriangles().CountTriplets(values);
+            tripletsNumber.Should().Be(34);
+        }
+
+    }
+}
---
+++ Codility.Solvers/CountTriangles.cs
@@ -0,0 +1,62 @@
+﻿using MoreLinq;
+using System.Linq;
+
+namespace Codility.Solvers
+{
+    public class CountTriangles
+    {
+        public int CountTriplets(int[] values)
+        {
+            var sortedValues = values.OrderBy(v => v).ToArray();
+            return CountFirstValuesNumber(sortedValues);
+        }
+
+        private int CountFirstValuesNumber(int[] values)
+        {
+            var counter = 0;
+
+            for (int i = 0; i < values.Length - 2; i++)
+            {
+                var first = values[i];
+                var secondValuesNumber = CountSecondValuesNumber(values, i + 1, first);
+                counter += secondValuesNumber;
+            }
+
+            return counter;
+        }
+
+        private int CountSecondValuesNumber(int[] values, int start, int first)
+        {
+            var secondValuesNumber = 0;
+            for (int j = start; j < values.Length - 1; j++)
+            {
+                var second = values[j];
+                var thirdValuesNumber = CountThirdValues(values, j + 1, first, second);
+                if (thirdValuesNumber == 0)
+                {
+                    break;
+                }
+
+                secondValuesNumber += thirdValuesNumber;
+            }
+
+            return secondValuesNumber;
+        }
+
+        private int CountThirdValues(int[] values, int start, int first, int second)
+        {
+            var thirdValuesNumber = 0;
+            for (int k = start; k < values.Length; k++)
+            {
+                var third = values[k];
+                if (first + second <= third)
+                {
+                    break;
+                }
+                thirdValuesNumber++;
+            }
+
+            return thirdValuesNumber;
+        }
+    }
+}
