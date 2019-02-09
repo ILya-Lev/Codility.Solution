2a91d35edfb54c3a08354bd574690731abf0f63e
Illia Levandovskyi <lis2889@gmail.com>
2019-02-09 01:07:37 +0100

    lesson 15 CountDistinctSlices - improved performance large range tests, length = ~100,000 got 705082704 expected 1Billion

--- Codility.Solvers/CountDistinctSlices.cs
+++ Codility.Solvers/CountDistinctSlices.cs
@@ -10,22 +10,22 @@ namespace Codility.Solvers
             if (sequence.Length == 0)
                 return 0;
 
-            var totalSlices = 0;
-            var slice = new Dictionary<int, int>();
+            var totalSlices = 0L;
+            var slice = new Slice();
             slice.Add(sequence[0], 0);
             for (int i = 1; i < sequence.Length; i++)
             {
-                if (slice.ContainsKey(sequence[i]))
+                if (slice.ContainsValue(sequence[i]))
                 {
                     totalSlices += GetNumberOfSubSlices(slice.Count);
-                    slice = ClearUpToKey(slice, sequence[i]);
+                    slice.CleanUpToValue(sequence[i]);
                     totalSlices -= GetNumberOfSubSlices(slice.Count);
                 }
                 slice.Add(sequence[i], i);
             }
             totalSlices += GetNumberOfSubSlices(slice.Count);
 
-            return totalSlices;
+            return (int)totalSlices;
         }
 
         private Dictionary<int, int> ClearUpToKey(Dictionary<int, int> slice, int key)
@@ -34,9 +34,38 @@ namespace Codility.Solvers
                 .ToDictionary(pair => pair.Key, pair => pair.Value);
         }
 
-        private int GetNumberOfSubSlices(int sequenceLength)
+        private long GetNumberOfSubSlices(int sequenceLength)
         {
-            return sequenceLength * (sequenceLength + 1) / 2;
+            return (long)sequenceLength * (sequenceLength + 1) / 2;
+        }
+
+        private class Slice
+        {
+            private readonly Dictionary<int, int> _valueByIndex = new Dictionary<int, int>();
+            private readonly Dictionary<int, int> _indexByValue = new Dictionary<int, int>();
+            private int _minIndex = 0;
+
+            public int Count => _valueByIndex.Count;
+
+            public bool ContainsValue(int value) => _valueByIndex.ContainsKey(value);
+
+            public void Add(int value, int index)
+            {
+                _valueByIndex.Add(value, index);
+                _indexByValue.Add(index, value);
+            }
+
+            public void CleanUpToValue(int value)
+            {
+                var targetIndex = _valueByIndex[value];
+                for (int i = targetIndex; i >= _minIndex; i--)
+                {
+                    _valueByIndex.Remove(_indexByValue[i]);
+                    _indexByValue.Remove(i);
+                }
+
+                _minIndex = targetIndex + 1;
+            }
         }
     }
 }
