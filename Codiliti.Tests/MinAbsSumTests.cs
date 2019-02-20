5fa8e400ef31647bf59802fea3d5fb4fc60e5db3
Illia Levandovskyi <lis2889@gmail.com>
2019-02-20 02:08:43 +0200

    lesson 17 MinAbsSum - 63% - 33345 case is not handled as process is not markovian

---
+++ Codiliti.Tests/MinAbsSumTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Codility.Solvers;
+using FluentAssertions;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class MinAbsSumTests
+    {
+        [Fact]
+        public void GetMinAbsTotal_Sample_0()
+        {
+            var values = new[] { 1, 5, -2, 2 };
+            var total = new MinAbsSum().GetMinAbsTotal(values);
+            total.Should().Be(0);
+        }
+
+        [Fact]
+        public void GetMinAbsTotal_Sample1_0()
+        {
+            var values = new[] { 3,3,3,4,5 };
+            var total = new MinAbsSum().GetMinAbsTotal(values);
+            total.Should().Be(0);
+        }
+
+        [Fact]
+        public void GetMinAbsTotal_2to20_1()
+        {
+            var values = Enumerable.Range(2,19).ToArray();
+            var total = new MinAbsSum().GetMinAbsTotal(values);
+            total.Should().Be(1);
+        }
+
+        [Fact]
+        public void GetMinAbsTotal_One_ItsAbs()
+        {
+            var values = new[] { -123 };
+            var total = new MinAbsSum().GetMinAbsTotal(values);
+            total.Should().Be(123);
+        }
+
+        [Fact]
+        public void GetMinAbsTotal_Empty_0()
+        {
+            var values = new int[0];
+            var total = new MinAbsSum().GetMinAbsTotal(values);
+            total.Should().Be(0);
+        }
+
+        [Fact]
+        public void GetMinAbsTotal_AllTheSameEvenAmount_0()
+        {
+            var random = new Random(DateTime.UtcNow.Millisecond);
+            var values = Enumerable.Repeat(1, 100_000).Select(n => random.Next(0, 2) == 0 ? -n : n).ToArray();
+            var total = new MinAbsSum().GetMinAbsTotal(values);
+            total.Should().Be(0);
+        }
+
+        [Fact]
+        public void GetMinAbsTotal_AllTheSameOddAmount_Item()
+        {
+            var random = new Random(DateTime.UtcNow.Millisecond);
+            var values = Enumerable.Repeat(7, 99_999).Select(n => random.Next(0, 2) == 0 ? -n : n).ToArray();
+            var total = new MinAbsSum().GetMinAbsTotal(values);
+            total.Should().Be(7);
+        }
+
+        [InlineData(10)]
+        [InlineData(11)]
+        [InlineData(100)]
+        [Theory]
+        public void GetMinAbsTotal_NaturalSequence_MaxDiv2Mod2(int amount)
+        {
+            var random = new Random(DateTime.UtcNow.Millisecond);
+            var values = Enumerable.Range(1, amount).Select(n => random.Next(0, 2) == 0 ? -n : n).ToArray();
+            var total = new MinAbsSum().GetMinAbsTotal(values);
+            total.Should().Be((amount + 1) / 2 % 2);
+        }
+    }
+}
---
+++ Codility.Solvers/MinAbsSum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codility.Solvers
+{
+    public class MinAbsSum
+    {
+        public int GetMinAbsTotal(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0) return 0;
+
+            var descendingAbs = ToDescendingAbs(numbers);
+            var minAbsTotal = TraverseOptimalPath(descendingAbs);
+
+            return minAbsTotal;
+        }
+
+        private int TraverseOptimalPath(int[] descendingAbs)
+        {
+            var state = new State(0, descendingAbs[0]);
+            foreach (var step in descendingAbs.Skip(1))
+            {
+                var nextValue = state.GetNextValue();
+                state = new State(nextValue, step);
+            }
+
+            return state.GetNextValue();
+        }
+
+
+        private int[] ToDescendingAbs(int[] numbers) => numbers.Select(Math.Abs).OrderByDescending(n => n).ToArray();
+
+        private class State
+        {
+            private readonly int _value;
+            private readonly int _step;
+
+            public State(int value, int step)
+            {
+                _value = value;
+                _step = step;
+            }
+
+            public int GetNextValue()
+            {
+                var left = Math.Abs(_value + _step);
+                var right = Math.Abs(_value - _step);
+
+                return Math.Min(left, right);
+            }
+        }
+    }
+}
