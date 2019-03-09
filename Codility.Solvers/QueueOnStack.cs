fbf96679eab286577b8c7e57051de7d35ab5f697
Illia Levandovskyi <lis2889@gmail.com>
2019-03-09 23:28:39 +0200

    another implementation

--- Codility.Solvers/QueueOnStack.cs
+++ Codility.Solvers/QueueOnStack.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Codility.Solvers
 {
@@ -14,12 +15,6 @@ namespace Codility.Solvers
 
         public void Enqueue(T item)
         {
-            if (_buffer.Count == 0)
-            {
-                _insertion.Push(item);
-                return;
-            }
-
             while (_buffer.Count > 0)
             {
                 _insertion.Push(_buffer.Pop());
@@ -40,5 +35,80 @@ namespace Codility.Solvers
             }
             return _insertion.Pop();
         }
+    }
+
+    /// <summary>
+    /// my implementation is 2 times faster. why?
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+
+    //public class QueueOnStack<T>
+    //{
+    //    private readonly DoubleStackQueue<T> _doubleStackQueue;
+
+    //    public QueueOnStack()
+    //    {
+    //        _doubleStackQueue = new DoubleStackQueue<T>();
+    //    }
+    //    public int Count => _doubleStackQueue.Count;
+
+    //    public void Enqueue(T item)
+    //    {
+    //        _doubleStackQueue.Enqueue(item);
+    //    }
+
+    //    public T Dequeue()
+    //    {
+    //        return _doubleStackQueue.Decueue();
+    //    }
+    //}
+
+    public class DoubleStackQueue<T>
+    {
+        public DoubleStackQueue()
+        {
+            PrimaryStorage = new Stack<T>();
+            SecondaryStorage = new Stack<T>();
+        }
+        private Stack<T> PrimaryStorage { get; set; }
+        private Stack<T> SecondaryStorage { get; set; }
+        public int Count => PrimaryStorage.Count + SecondaryStorage.Count;
+
+        public bool IsEmpty() => PrimaryStorage.IsEmpty() && SecondaryStorage.IsEmpty();
+
+        private T Take(Func<T> storageAccessor)
+        {
+            if (SecondaryStorage.IsEmpty())
+                PrimaryStorage.ReverseTo(SecondaryStorage);
+
+            return storageAccessor.Invoke();
+        }
+             
+        public void Enqueue(T item)
+        {
+            if (PrimaryStorage.IsEmpty())
+                SecondaryStorage.ReverseTo(PrimaryStorage);
+
+            PrimaryStorage.Push(item);
+        }
+
+        public T Decueue() => Take(() => SecondaryStorage.Pop());
+
+        public T Peek() => Take(() => SecondaryStorage.Peek());
+    }
+
+    public static class StackExtensions
+    {
+        public static Stack<T> ReverseTo<T>(this Stack<T> source, Stack<T> destination)
+        {
+            while (!source.IsEmpty())
+            {
+                destination.Push(source.Pop());
+            }
+
+            return destination;
+        }
+
+        public static bool IsEmpty<T>(this Stack<T> source) => source.Count == 0;
     }
 }
