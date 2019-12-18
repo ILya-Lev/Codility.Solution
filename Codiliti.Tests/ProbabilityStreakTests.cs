e2d7875b6b20011cd1b354d93ba99ee898b17f41
Illia Levandovskyi <lis2889@gmail.com>
2019-12-18 23:01:36 +0200

    multiplicative probability streak tests

---
+++ Codiliti.Tests/ProbabilityStreakTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Codility.Solvers;
+using FluentAssertions;
+using MoreLinq;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Codility.Tests
+{
+    public class ProbabilityStreakTests : IClassFixture<TestOutputHelper>
+    {
+        private readonly TestOutputHelper _output;
+        public ProbabilityStreakTests(TestOutputHelper output)  { _output = output; }
+
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1_000)]
+        [InlineData(10_000)]
+        [Theory]
+        public void DoStep_NSteps_ObserveResult(int n)
+        {
+            var streak = new ProbabilityStreak(0.5, 0.4);
+            var seed = 100;
+            var sequence = new List<double>(n);
+
+            double current = seed;
+            for (int i = 0; i < n; i++)
+            {
+                sequence.Add(current);
+                if (i % 1_000 == 0)
+                    _output.WriteLine($"i = {i} => current = {current}");
+
+                current = streak.DoStep(current);
+            }
+
+            _output.WriteLine($"{n}th value {sequence.Last()}");
+            sequence.Last().Should().BeGreaterOrEqualTo(0);
+        }
+
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1_000)]
+        [InlineData(10_000)]
+        [Theory]
+        public void DoStep_Ensemble_ObserveResult(int n)
+        {
+            var streakGenerators = new List<ProbabilityStreak>(n);
+            for (int i = 0; i < n; i++)
+            {
+                streakGenerators.Add(new ProbabilityStreak(0.5, 0.4));
+            }
+
+            var current = Enumerable.Repeat(100.0, n).ToArray();
+            var lenght = 100;
+
+            for (int i = 0; i < lenght; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    current[j] = streakGenerators[j].DoStep(current[j]);
+                    //if (i != 0 && i % 1_000 == 0)
+                    //    _output.WriteLine($"i={i}, j={j}, current = {current[j]}");
+                }
+            }
+
+            //foreach (var c in current) _output.WriteLine($"current {c}");
+
+            _output.WriteLine($"average ensemble {current.Average()}");
+            _output.WriteLine($"max ensemble {current.Max()}");
+            _output.WriteLine($"min ensemble {current.Min()}");
+        }
+    }
+}
---
+++ Codility.Solvers/ProbabilityStreak.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Codility.Solvers
+{
+    public class ProbabilityStreak
+    {
+        private readonly double _stepUp;
+        private readonly double _stepDown;
+        private readonly Random _randomGen = new Random(DateTime.UtcNow.Millisecond);
+
+        public ProbabilityStreak(double stepUp, double stepDown)
+        {
+            _stepUp = Math.Abs(stepUp);
+            _stepDown = Math.Abs(stepDown);
+        }
+
+        public double DoStep(double seed)
+        {
+            var factor = _randomGen.Next(0, 10) % 2 == 1 ? (1 + _stepUp) : (1 - _stepDown);
+            return seed * factor;
+        }
+    }
+}
