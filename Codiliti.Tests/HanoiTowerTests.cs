582f3d60979f97d343689d8dc720bd684653ef2b
tillileva <tillileva@SUBDN457>
2020-01-03 11:04:21 +0100

    Hanoi tower (classical case) recursive solution

---
+++ Codiliti.Tests/HanoiTowerTests.cs
@@ -0,0 +1,37 @@
+﻿using Codility.Solvers.Hanoi;
+using FluentAssertions;
+using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Codility.Tests
+{
+    public class HanoiTowerTests : IClassFixture<TestOutputHelper>
+    {
+        private readonly TestOutputHelper _output;
+
+        public HanoiTowerTests(TestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [Theory]
+        public void Move_NDisks_InOptimalWay(in int n)
+        {
+            var tower = new HanoiTower();
+            
+            var steps = tower.Move(n, 1, 2, 3);
+            
+            steps.Should().HaveCount((int)Math.Pow(2,n) - 1);
+            foreach (var step in steps)
+            {
+                _output.WriteLine($"disk # {step.DiskNumber} goes from {step.OriginStick} to {step.TargetStick}");
+            }
+        }
+    }
+}
---
+++ Codility.Solvers/HanoiTower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Codility.Solvers.Hanoi
+{
+    public class Step
+    {
+        public int DiskNumber { get; set; }
+        public int OriginStick { get; set; }
+        public int TargetStick { get; set; }
+    }
+
+    public class HanoiTower
+    {
+        public IReadOnlyList<Step> Move(in int n, in int origin, in int buffer, in int target)
+        {
+            if (n < 1) return new Step[0];
+
+            var steps = new List<Step>();
+            
+            steps.AddRange(Move(n-1, origin, target, buffer));
+            steps.Add(new Step(){DiskNumber = n, OriginStick = origin, TargetStick = target});
+            steps.AddRange(Move(n-1, buffer, origin, target));
+            
+            return steps;
+        }
+    }
+}
