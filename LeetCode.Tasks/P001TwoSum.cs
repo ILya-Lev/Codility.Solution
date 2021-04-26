952aeb59c20efff4c94c945ca6516f87dc83bd6e
Illia Levandovskyi <lis2889@gmail.com>
2021-04-26 12:05:03 +0300

    leet code the very first problem ever

---
+++ LeetCode.Tasks/P001TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Tasks
+{
+    public class P001TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var lookup = nums.Select((n, idx) => new { n, idx })
+                .GroupBy(p => p.n, p => p.idx)
+                .ToDictionary(g => g.Key, g => g.ToArray());
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var secondAddition = target - nums[i];
+                if (lookup.TryGetValue(secondAddition, out var secondIndexes) && secondIndexes.Length > 0)
+                {
+                    var secondIndex = secondIndexes.FirstOrDefault(idx => idx > i);
+                    if (secondIndex != 0)
+                        return new[] { i, secondIndex };
+                }
+            }
+
+            throw new KeyNotFoundException();
+        }
+    }
+}
---
+++ LeetCode.Tests/P001TwoSumTests.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using LeetCode.Tasks;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    [Trait("Category", "Unit")]
+    public class P001TwoSumTests
+    {
+        private readonly P001TwoSum _sut = new P001TwoSum();
+
+        [Fact]
+        public void TwoSum_NoDuplicates_ContainSolution()
+        {
+            var numbers = new[] { 9, 7, 3, 1, 6, 4, 2 };
+            _sut.TwoSum(numbers, 13).Should().Equal(new[] { 0, 5 });        //values 9 and 4 instead of 7 and 6
+        }
+
+        [Fact]
+        public void TwoSum_WithDuplicates_ContainSolution()
+        {
+            var numbers = new[] { 3, 3, 3 };
+            _sut.TwoSum(numbers, 6).Should().Equal(new[] { 0, 1 });
+        }
+
+        [Fact]
+        public void TwoSum_DoesNotContainSolution_Exception()
+        {
+            var numbers = new[] { 9, 7, 3, 1, 6, 4, 2 };
+            Action getTwoSum = () => _sut.TwoSum(numbers, 500);
+            getTwoSum.Should().Throw<KeyNotFoundException>();
+        }
+    }
+}
