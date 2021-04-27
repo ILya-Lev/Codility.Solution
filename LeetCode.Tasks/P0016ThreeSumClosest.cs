25e47f78edca7b1e5ec2d9358bfdd5056ec66753
Illia Levandovskyi <lis2889@gmail.com>
2021-04-28 01:22:31 +0300

    p 16 solved - accepted

---
+++ LeetCode.Tasks/P0016ThreeSumClosest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Tasks
+{
+    public class P0016ThreeSumClosest
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            if (nums.Length < 3) throw new ArgumentException("input array is too short", nameof(nums));
+
+            if (nums.Length == 3) return nums.Sum();
+
+            var sorted = nums.OrderBy(n => n).ToList();
+            var closestSum = sorted.Take(3).Sum();
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                for (int j = i + 1; j < sorted.Count; j++)
+                {
+                    var compliment = target - sorted[i] - sorted[j];
+
+                    var complimentIndex = sorted.BinarySearch(compliment);
+                    if (complimentIndex < 0)
+                        complimentIndex = Math.Max(Math.Min(~complimentIndex - 1, sorted.Count - 1), 0);
+                    if (complimentIndex == i)
+                        complimentIndex++;
+                    if (complimentIndex == j)
+                        complimentIndex++;
+                    if (complimentIndex >= sorted.Count)
+                        continue;//or break?
+
+                    var closestCompliment = sorted[complimentIndex];
+                    var currentSum = target - compliment + closestCompliment;
+
+                    closestSum = Math.Abs(closestCompliment - compliment) < Math.Abs(target - closestSum)
+                        ? currentSum
+                        : closestSum;
+                    if (closestSum == target)
+                        return target;
+                }
+            }
+
+            return closestSum;
+        }
+    }
+}
---
+++ LeetCode.Tests/P0016ThreeSumClosestTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using System;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Tests
+{
+    [Trait("Category", "Unit")]
+    public class P0016ThreeSumClosestTests
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly P0016ThreeSumClosest _sut = new P0016ThreeSumClosest();
+
+        public P0016ThreeSumClosestTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void ThreeSumClosest_SimpleValid_Find()
+        {
+            var nums = new[] { -1, 2, 1, -4 };
+            var sum = _sut.ThreeSumClosest(nums, 1);
+            sum.Should().Be(2);
+        }
+
+        [Fact]
+        public void ThreeSumClosest_SimpleValid01_Find()
+        {
+            var nums = new[] { -1, 0, 1, 1, 55 };
+            var sum = _sut.ThreeSumClosest(nums, 3);
+            sum.Should().Be(2);
+        }
+
+        [Fact]
+        public void ThreeSumClosest_BigRandom_Find()
+        {
+            var random = new Random(DateTime.UtcNow.Millisecond);
+            var nums = Enumerable.Range(3, 1_000).Select(n => random.Next(-1000, 1000)).ToArray();
+            var target = random.Next(-10_000, 10_000);
+            var sum = _sut.ThreeSumClosest(nums, target);
+            _output.WriteLine($"for target {target} the closest sum is {sum}");
+        }
+    }
+}
