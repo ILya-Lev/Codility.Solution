27240a558a5f709295e034e5adcd8520b5e796f7
Illia Levandovskyi <lis2889@gmail.com>
2021-05-02 23:30:33 +0300

    problem # 2 solved - add two numbers as linked lists

---
+++ LeetCode.Tasks/P0002AddTwoNumbers.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Tasks
+{
+    public class ListNode //: IEquatable<ListNode>
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int value = 0, ListNode nextNode = null)
+        {
+            val = value;
+            next = nextNode;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ListNode node && Equals(node);
+        }
+
+        public bool Equals(ListNode other)
+        {
+            var thisIterator = this;
+            var otherIterator = other;
+            while (thisIterator != null && otherIterator != null)
+            {
+                if (thisIterator.val != otherIterator.val)
+                    return false;
+                thisIterator = thisIterator.next;
+                otherIterator = otherIterator.next;
+            }
+
+            return thisIterator == null && otherIterator == null;
+        }
+    }
+
+    public static class ListNodeExtensions
+    {
+        public static ListNode FromDirectOrder(this IReadOnlyList<int> source)
+        {
+            var number = new ListNode();
+            for (int i = 0; i < source.Count; i++)
+            {
+                number.val = source[i];
+                if (i + 1 == source.Count)
+                    return number;
+
+                var previous = new ListNode(nextNode: number);
+                number = previous;
+            }
+
+            return number;//actually will never rich here
+        }
+    }
+
+    public class P0002AddTwoNumbers
+    {
+        public ListNode AddTwoNumbers(ListNode lhs, ListNode rhs)
+        {
+            var tail = new ListNode();
+            var head = tail;
+            var nextDigitDelta = 0;
+            while (lhs != null && rhs != null)
+            {
+                var currentSum = lhs.val + rhs.val + nextDigitDelta;
+
+                tail.val = currentSum % 10;
+                tail.next = new ListNode();
+                tail = tail.next;
+
+                nextDigitDelta = currentSum / 10;
+                lhs = lhs.next;
+                rhs = rhs.next;
+            }
+
+            tail = TraverseRemainder(lhs, ref nextDigitDelta, tail);
+            tail = TraverseRemainder(rhs, ref nextDigitDelta, tail);
+
+            if (nextDigitDelta != 0)
+                tail.val = nextDigitDelta;
+            else
+                RemoveTrailingZero(head, tail);
+
+            return head;
+        }
+
+        private static ListNode TraverseRemainder(ListNode remainder, ref int nextDigitDelta, ListNode tail)
+        {
+            if (remainder == null)
+                return tail;
+
+            while (remainder != null)
+            {
+                var currentSum = remainder.val + nextDigitDelta;
+
+                tail.val = currentSum % 10;
+                tail.next = new ListNode();
+                tail = tail.next;
+
+                nextDigitDelta = currentSum / 10;
+                remainder = remainder.next;
+            }
+
+            if (nextDigitDelta != 0)
+            {
+                tail.val = nextDigitDelta;
+                nextDigitDelta = 0;
+            }
+
+            return tail;
+        }
+
+        private void RemoveTrailingZero(ListNode head, ListNode tail)
+        {
+            if (!head.Equals(tail) && tail.val == 0)
+            {
+                var current = head;
+                while (current.next.next != null)
+                {
+                    current = current.next;
+                }
+
+                current.next = null;
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/P0002AddTwoNumbersTests.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    [Trait("Category", "Unit")]
+    public class P0002AddTwoNumbersTests
+    {
+        private readonly P0002AddTwoNumbers _sut = new();
+
+        [Fact]
+        public void AddTwoNumbers_TheSameLength_SimpleAddition()
+        {
+            var first = new[] { 3, 4, 2 }.FromDirectOrder();
+            var second = new[] { 4, 6, 5 }.FromDirectOrder();
+            var expected = new[] { 8, 0, 7 }.FromDirectOrder();
+
+            var total = _sut.AddTwoNumbers(first, second);
+
+            total.Should().Be(expected);
+        }
+
+        [Fact]
+        public void AddTwoNumbers_TwoZeros_Zero()
+        {
+            var first = new[] { 0 }.FromDirectOrder();
+            var second = new[] { 0 }.FromDirectOrder();
+            var expected = new[] { 0 }.FromDirectOrder();
+
+            var total = _sut.AddTwoNumbers(first, second);
+
+            total.Should().Be(expected);
+        }
+
+        [Fact]
+        public void AddTwoNumbers_5And5_Ten()
+        {
+            var first = new[] { 5 }.FromDirectOrder();
+            var second = new[] { 5 }.FromDirectOrder();
+            var expected = new[] { 1, 0 }.FromDirectOrder();
+
+            var total = _sut.AddTwoNumbers(first, second);
+
+            total.Should().Be(expected);
+        }
+
+        [Fact]
+        public void AddTwoNumbers_Many9s_PropagateOne()
+        {
+            var first = new[] { 9, 9, 9, 9, 9, 9, 9 }.FromDirectOrder();
+            var second = new[] { 9, 9, 9, 9 }.FromDirectOrder();
+            var expected = new[] { 1, 0, 0, 0, 9, 9, 9, 8 }.FromDirectOrder();
+
+            var total = _sut.AddTwoNumbers(first, second);
+
+            total.Equals(expected).Should().BeTrue();
+        }
+
+        [Fact]
+        public void AddTwoNumbers_Simple002_PropagateOne()
+        {
+            var first = new[] { 9, 4, 2 }.FromDirectOrder();
+            var second = new[] { 9, 4, 6, 5 }.FromDirectOrder();
+            var expected = new[] { 1, 0, 4, 0, 7 }.FromDirectOrder();
+
+            var total = _sut.AddTwoNumbers(first, second);
+
+            total.Equals(expected).Should().BeTrue();
+        }
+    }
+}
