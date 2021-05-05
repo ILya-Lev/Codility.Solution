d2561c1815928504c81e94469462043325d0177f
Illia Levandovskyi <lis2889@gmail.com>
2021-05-05 23:40:39 +0300

    p 403 frog jumps - naive implementation - works but for small cases only - up to 30 stones

---
+++ LeetCode.Tasks/P0403FrogJump.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Tasks
+{
+    public class P0403FrogJump
+    {
+        public int TryJumpCounter { get; set; }
+        /// <summary>
+        /// preconditions:
+        /// 1. input array is sorted in an ascending order
+        /// 2. there are at least 2 stones
+        /// 3. first stone has to be 0
+        /// </summary>
+        /// <param name="stones"></param>
+        /// <returns></returns>
+        public bool CanCross(int[] stones)
+        {
+            if (stones?.Length >= 2 == false || stones[0] != 0)
+                return false;
+
+            if (!CanDoFirstJump(stones))
+                return false;
+
+            if (!CanCoverLastStone(stones))
+                return false;
+
+            return FindOutIfCanCross(stones.ToList(), 1, 1);
+        }
+
+        private static bool CanDoFirstJump(IReadOnlyList<int> stones) => stones[1] == 1;
+
+        private bool CanCoverLastStone(IReadOnlyCollection<int> stones) => stones.Last() <= stones.Count * (stones.Count - 1) / 2;
+
+        private bool FindOutIfCanCross(List<int> stones, int position, int jumpSize)
+        {
+            if (stones.Count - 1 == position) //standing on the last stone
+                return true;
+
+            return TryJump(stones, position, jumpSize - 1)
+                || TryJump(stones, position, jumpSize)
+                || TryJump(stones, position, jumpSize + 1);
+        }
+
+        private bool TryJump(List<int> stones, int position, int jumpSize)
+        {
+            if (jumpSize < 1) return false;
+
+            TryJumpCounter++;
+            var nextStoneValue = stones[position] + jumpSize;
+            var nextPosition = stones.BinarySearch(nextStoneValue);
+
+            if (nextPosition > position && nextPosition < stones.Count)
+                return FindOutIfCanCross(stones, nextPosition, jumpSize);   //recursive call
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode.Tests/P0403FrogJumpTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using System.Linq;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    [Trait("Category", "Unit")]
+    public class P0403FrogJumpTests
+    {
+        private readonly P0403FrogJump _sut = new();
+
+        [Fact]
+        public void CanCross_SkipOneStoneInTheMiddle_True()
+        {
+            var stones = new[] { 0, 1, 3, 5, 6, 8, 12, 17 };
+            _sut.CanCross(stones).Should().BeTrue();
+        }
+
+        [Fact]
+        public void CanCross_TooBigGapInTheMiddle_False()
+        {
+            var stones = new[] { 0, 1, 2, 3, 4, 8, 9, 11 };
+            _sut.CanCross(stones).Should().BeFalse();
+        }
+
+        [Fact]
+        public void CanCross_Empty_False()
+        {
+            var stones = new int[0];
+            var stones1 = new[] { 0 };
+            var stones2 = new[] { 1 };
+            _sut.CanCross(stones).Should().BeFalse();
+            _sut.CanCross(stones1).Should().BeFalse();
+            _sut.CanCross(stones2).Should().BeFalse();
+        }
+
+        [Fact]
+        public void CanCross_TooBigFirstJump_False()
+        {
+            var stones = new[] { 0, 2 };
+            _sut.CanCross(stones).Should().BeFalse();
+        }
+
+        [Fact(Timeout = 10)]
+        public void CanCross_ManyStones_False()
+        {
+            //var a = 998;//last consecutive item
+            var a = 30;//last consecutive item
+            //var d = 37;//delta between the very last and the last but one items
+            var d = 37;//delta between the very last and the last but one items
+            var stones = Enumerable.Range(0, a + 1).Concat(new[] { a + d }).ToArray();
+            stones[^2].Should().Be(a);
+            (stones[^1] - stones[^2]).Should().Be(d);
+
+            //var deltaNotOne = new List<int>();
+            //for (int i = 0; i + 1 < stones.Length; i++)
+            //{
+            //    if (stones[i] + 1 != stones[i + 1])
+            //        deltaNotOne.Add(stones[i + 1]);
+            //}
+
+            //deltaNotOne.Should().HaveCount(1);
+            //deltaNotOne[0].Should().Be(1035);
+
+            _sut.CanCross(stones).Should().BeFalse();
+            _sut.TryJumpCounter.Should().BeLessThan(a * 5);
+        }
+    }
+}
