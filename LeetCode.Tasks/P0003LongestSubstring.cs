a4245f46c080679051b1ede6818b7e65e1437738
Illia Levandovskyi <lis2889@gmail.com>
2021-05-07 14:38:16 +0300

    p 0003 unique characters substring - accepted

---
+++ LeetCode.Tasks/P0003LongestSubstring.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Tasks
+{
+    public class P0003LongestSubstring
+    {
+        public int FindLongestUniqueLength(string s)
+        {
+            var maxLength = 0;
+            for (int start = 0; start < s.Length; start++)
+            {
+                var currentLength = GetUniqueSubstringLength(s, start);
+                maxLength = Math.Max(maxLength, currentLength);
+            }
+            return maxLength;
+        }
+
+        private int GetUniqueSubstringLength(string s, int start)
+        {
+            var seenCharacters = new HashSet<char>();
+            for (int i = start; i < s.Length; i++)
+            {
+                if (!seenCharacters.Add(s[i]))  //returns false if already present
+                    return i - start;
+            }
+            return s.Length - start;
+        }
+    }
+}
---
+++ LeetCode.Tests/P0003LongestSubstringTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class P0003LongestSubstringTests
+    {
+        private readonly P0003LongestSubstring _sut = new P0003LongestSubstring();
+
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        public void FindLongestUniqueLength_ValidInput_MatchExpectations(string s, int expectedLength)
+        {
+            _sut.FindLongestUniqueLength(s).Should().Be(expectedLength);
+        }
+    }
+}
