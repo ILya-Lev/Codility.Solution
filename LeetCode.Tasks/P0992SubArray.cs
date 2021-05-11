9c8e6b9d2b04787bc9df951f0e40c43172d30c4e
Illia Levandovskyi <lis2889@gmail.com>
2021-05-11 15:06:41 +0300

    p 0992 (hard) accepted, 216 ms beats 11% of others => there must be conceptually another solution - best cases are about 2/3 of this time

---
+++ LeetCode.Tasks/P0992SubArray.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Tasks
+{
+    public class P0992SubArray
+    {
+        private readonly List<int> _subArray = new List<int>();
+        private readonly Dictionary<int, int> _popularity = new Dictionary<int, int>();
+
+        public int SubarraysWithKDistinct(int[] source, int k)
+        {
+            _subArray.Clear();
+            _popularity.Clear();
+
+            var counter = 0;
+
+            foreach (var number in source)
+            {
+                if (_popularity.Count < k)
+                {
+                    AppendToSubArray(number);
+
+                    if (_popularity.Count == k)
+                    {
+                        counter += GetValidSubArrayAmount(_subArray, _popularity);
+                    }
+
+                    continue;
+                }
+
+                if (_popularity.Count == k)
+                {
+                    AppendToSubArray(number);
+
+                    if (_popularity.Count == k)
+                    {
+                        counter += GetValidSubArrayAmount(_subArray, _popularity);
+                    }
+                    else// if (_popularity.Count > k)
+                    {
+                        ShrinkSubArrayFromStart(k);
+                        if (_popularity.Count == k)
+                        {
+                            counter += GetValidSubArrayAmount(_subArray, _popularity);
+                        }
+                    }
+                }
+            }
+
+            return counter;
+        }
+
+        /// <summary>
+        /// having sub array with k+1 unique numbers inside remove numbers in front of it
+        /// while the amount of unique numbers is not k again
+        /// </summary>
+        private void ShrinkSubArrayFromStart(in int k)
+        {
+            for (int i = 0; i < _subArray.Count; i++)
+            {
+                var head = _subArray[i];
+                _popularity[head]--;
+                _subArray.RemoveAt(i--);//to cancel up i++ in the end of the loop body
+
+
+                if (_popularity[head] <= 0)
+                {
+                    _popularity.Remove(head);
+                    if (_popularity.Count <= k)
+                        return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// not only increase the sub array, but also the statistics - popularity of given number
+        /// </summary>
+        private void AppendToSubArray(int number)
+        {
+            _subArray.Add(number);
+            _popularity[number] = _popularity.TryGetValue(number, out var currentPopularity)
+                ? currentPopularity + 1
+                : 1;
+        }
+
+        /// <summary>
+        /// emulate removing by 1 item on each loop step in front of the sub array while it's still valid
+        /// due to amount of unique items inside
+        /// is static and except read only collections as should not change the state of the solver
+        /// </summary>
+        private static int GetValidSubArrayAmount(IReadOnlyCollection<int> subArray, IReadOnlyDictionary<int, int> popularity)
+        {
+            var counter = 1;    //for initial subArray
+            var popularityOfShrink = new Dictionary<int, int>();
+
+            foreach (var head in subArray)
+            {
+                popularityOfShrink[head] = popularityOfShrink.TryGetValue(head, out var currentPopularity)
+                    ? currentPopularity + 1
+                    : 1;
+
+                if (popularity[head] <= popularityOfShrink[head])
+                    return counter;
+
+                counter++;
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ LeetCode.Tests/P0992SubArrayTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using System.Linq;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class P0992SubArrayTests
+    {
+        [Fact]
+        public void SubArraysWithKDistinct_AllTheSame_Kis2_Zero()
+        {
+            var sut = new P0992SubArray();
+            var source = Enumerable.Repeat(1, 5).ToArray();
+
+            sut.SubarraysWithKDistinct(source, 2)
+                .Should()
+                .Be(0, "source contains the same number repeated several times," +
+                       " while sub arrays should have exactly 2 different integers");
+        }
+
+        [Fact]
+        public void SubArraysWithKDistinct_AllTheSame_Kis1_15()
+        {
+            var sut = new P0992SubArray();
+            var source = Enumerable.Repeat(1, 5).ToArray();
+
+            sut.SubarraysWithKDistinct(source, 1)
+                .Should()
+                .Be(15, "all possible consequent sub array fits in");
+        }
+
+        [Theory]
+        [InlineData(1, 0)]
+        [InlineData(2, 1)]
+        [InlineData(1_000, 999)]
+        [InlineData(10_000, 9_999)]
+        [InlineData(20_000, 19_999)]
+        public void SubArraysWithKDistinct_AllUnique_Kis1_SizeOfSource(int k, int delta)
+        {
+            var sut = new P0992SubArray();
+            var size = 20_000;
+            var source = Enumerable.Range(1, size).ToArray();
+
+            sut.SubarraysWithKDistinct(source, k).Should().Be(size - delta);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 1, 2, 3 }, 2, 7)]
+        [InlineData(new[] { 1, 2, 1, 3, 4 }, 3, 3)]
+        public void SubArraysWithKDistinct_Sample001_Kis2_7(int[] source, int k, int expectedAmount)
+        {
+            new P0992SubArray().SubarraysWithKDistinct(source, k).Should().Be(expectedAmount);
+        }
+    }
+}
