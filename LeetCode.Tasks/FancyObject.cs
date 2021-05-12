d6718abefa9926af0df8cf6d55126314845e7a0a
Illia Levandovskyi <lis2889@gmail.com>
2021-05-12 23:15:34 +0300

    serialization check added

---
+++ LeetCode.Tasks/FancyObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeetCode.Tasks
+{
+    public class FancyObject
+    {
+        public int I { get; }
+        public string S { get; }
+        public DateTime Dt { get; set; }
+        public DayOfWeek DayOfWeek { get; }
+
+        public FancyObject(int i, string s, DayOfWeek dayOfWeek)
+        {
+            I = i;
+            S = s;
+            DayOfWeek = dayOfWeek;
+        }
+    }
+}
---
+++ LeetCode.Tests/FancyObjectTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using LeetCode.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Tests
+{
+    public class FancyObjectTests
+    {
+        private readonly ITestOutputHelper _output;
+        public FancyObjectTests(ITestOutputHelper output) => _output = output;
+
+        [Fact]
+        public void Serialize_BackAndForth_MatchItself()
+        {
+            var item = new Fixture().Create<FancyObject>();
+
+            var rawItem = JsonConvert.SerializeObject(item);
+            _output.WriteLine(rawItem);
+
+            var restoredItem = JsonConvert.DeserializeObject<FancyObject>(rawItem);
+            restoredItem.Should()
+                //.IsSameOrEqualTo(item);
+                .BeEquivalentTo(item);
+        }
+    }
+}
