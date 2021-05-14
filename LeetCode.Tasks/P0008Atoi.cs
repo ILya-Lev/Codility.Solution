5daf9ca6900e511054aefc51418375baccc7425d
Illia Levandovskyi <lis2889@gmail.com>
2021-05-14 18:39:29 +0300

    p 0008 integer parsing from an input string - accepted

---
+++ LeetCode.Tasks/P0008Atoi.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Tasks
+{
+    public class P0008Atoi
+    {
+        private static readonly IReadOnlyDictionary<char, int> _digitMap = new Dictionary<char, int>()
+        {
+            ['0'] = 0,
+            ['1'] = 1,
+            ['2'] = 2,
+            ['3'] = 3,
+            ['4'] = 4,
+            ['5'] = 5,
+            ['6'] = 6,
+            ['7'] = 7,
+            ['8'] = 8,
+            ['9'] = 9,
+        };
+
+        public int MyAtoi(string s)
+        {
+            int? sign = null;
+            int? number = null;
+            foreach (var c in s.ToCharArray())
+            {
+                if (c == '-')
+                {
+                    //stop parsing as either sign is met second time
+                    //or sign is met after the number
+                    if (sign.HasValue || number.HasValue) break;
+                    sign = -1;
+                    continue;
+                }
+
+                if (c == '+')
+                {
+                    //stop parsing as either sign is met second time
+                    //or sign is met after the number
+                    if (sign.HasValue || number.HasValue) break;
+                    sign = +1;
+                    continue;
+                }
+
+                if (c == ' ')
+                {
+                    //stop parsing as the white space is met either after a sign or a number
+                    if (sign.HasValue || number.HasValue) break;
+
+                    //skip leading whitespace, by design only ' '
+                    continue;
+                }
+
+                if (char.IsDigit(c))
+                {
+                    if (number is null)
+                    {
+                        number = _digitMap[c];
+                        continue;
+                    }
+
+                    if (sign != -1 && number > (int.MaxValue - _digitMap[c]) / 10)
+                    {
+                        number = int.MaxValue;
+                        //stop parsing as number has reached its upper boundary = int.max
+                        break;
+                    }
+
+                    if (sign == -1 && number > (int.MaxValue - _digitMap[c] + 1L) / 10)
+                    {
+                        number = int.MinValue;
+                        //stop parsing as number has reached its lower boundary = int.min
+                        break;
+                    }
+
+                    number = 10 * number + _digitMap[c];
+                    continue;
+                }
+
+                break;//stop parsing as soon as non digit or non whitespace is met
+            }
+
+            //if no sign is specified explicitly treat number as positive
+            //if no number has been parsed from the input return 0
+            return (sign ?? +1) * (number ?? 0);
+        }
+    }
+}
---
+++ LeetCode.Tests/P0008AtoiTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using System.Linq;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    [Trait("Category", "Unit")]
+    public class P0008AtoiTests
+    {
+        private readonly P0008Atoi _sut = new P0008Atoi();
+
+        [Theory]
+        [InlineData("string and 918261")]
+        [InlineData("\\t918261")]// \t instead of \s
+        [InlineData("--918261")]
+        [InlineData("-+918261")]
+        [InlineData("00000-654654")]
+        [InlineData("00000+654654")]
+        [InlineData("   +0 321")]
+        public void MyAtoi_Invalid_Zero(string input)
+        {
+            _sut.MyAtoi(input).Should().Be(0);
+        }
+
+        [Theory]
+        [InlineData("918261 and string", 918261)]
+        [InlineData("918261", 918261)]
+        [InlineData(" 918261", 918261)]
+        [InlineData("   -918261", -918261)]
+        [InlineData("   +918261", 918261)]
+        [InlineData("-5-", -5)]
+        public void MyAtoi_Valid_MatchExpectations(string input, int expected)
+        {
+            _sut.MyAtoi(input).Should().Be(expected);
+        }
+
+        [Fact]
+        public void MyAtoi_TooBig_StickToInt32Max()
+        {
+            var input = Enumerable.Repeat(4, 200)
+                .Select(n => n.ToString())
+                .Aggregate((current, next) => current + next);
+
+            _sut.MyAtoi(input).Should().Be(int.MaxValue);
+        }
+
+        [Fact]
+        public void MyAtoi_TooSmall_StickToInt32Min()
+        {
+            var input = Enumerable.Repeat(4, 200)
+                .Select(n => n.ToString())
+                .Aggregate((current, next) => current + next);
+
+            _sut.MyAtoi($"-{input}").Should().Be(int.MinValue);
+        }
+
+        [Fact]
+        public void MyAtoi_TooSmallWith0AtEdge_StickToInt32Min()
+        {
+            //2147483647 == int.max, next digit is 0, so int.max + next digit <= int.max; but the number itself is out of reach
+            _sut.MyAtoi("-21474836470").Should().Be(int.MinValue);
+        }
+    }
+}
