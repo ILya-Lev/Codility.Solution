7c533dd09591578203b296e304cc6a2f4f80f71f
Illia Levandovskyi <lis2889@gmail.com>
2021-05-15 18:57:18 +0300

    P 0007 reverse integer - accepted

---
+++ LeetCode.Tasks/P0007ReverseInteger.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Tasks
+{
+    public class P0007ReverseInteger
+    {
+        public int Reverse(in int x)
+        {
+            var initial = x;
+            var reversed = 0;
+            while (initial != 0)
+            {
+                if (initial > 0 && reversed > (int.MaxValue - initial % 10) / 10)
+                    return 0;
+
+                if (initial < 0 && reversed < (int.MinValue - initial % 10) / 10)
+                    return 0;
+
+                reversed = reversed * 10 + initial % 10;
+                initial /= 10;
+            }
+
+            return reversed;
+        }
+    }
+}
---
+++ LeetCode.Tests/P0007ReverseIntegerTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class P0007ReverseIntegerTests
+    {
+        private readonly P0007ReverseInteger _sut = new();
+
+        [Fact] public void Reverse_123_321() => _sut.Reverse(123).Should().Be(321);
+        [Fact] public void Reverse_120_21() => _sut.Reverse(120).Should().Be(021);
+        [Fact] public void Reverse_Negative123_PreserveSign() => _sut.Reverse(-123).Should().Be(-321);
+        [Fact] public void Reverse_Zero_Zero() => _sut.Reverse(-0).Should().Be(0);
+        [Fact] public void Reverse_IntMax_Zero() => _sut.Reverse(int.MaxValue).Should().Be(0);
+        [Fact] public void Reverse_IntMin_Zero() => _sut.Reverse(int.MinValue).Should().Be(0);
+        //-2147483648 min  126_087_180
+        //2147483647 max
+    }
+}
