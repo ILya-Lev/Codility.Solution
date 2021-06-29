91668764e7458ff5ce8522eaf33195dc7f26980d
Illia Levandovskyi <lis2889@gmail.com>
2021-06-30 00:36:31 +0300

    zig zag - not efficient, but accepted

---
+++ LeetCode.Tasks/P0006ZigZagConversion.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Tasks
+{
+    public class P0006ZigZagConversion
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (string.IsNullOrWhiteSpace(s) || numRows == 1) return s;
+
+            var charByNumber = GetCharByNumber(s, numRows);
+            var chars = new List<char>(s.Length);
+            for (int rowNumber = 1; rowNumber <= numRows; rowNumber++)
+            {
+                chars.AddRange(charByNumber.Where(item => item.r == rowNumber).Select(item => item.c));
+            }
+
+            return new string(chars.ToArray());
+        }
+
+        private static List<(char c, int r)> GetCharByNumber(string s, int numRows)
+        {
+            var charByNumber = new List<(char, int)>(s.Length);
+            bool isDown = true;
+            var rowNumber = 1;
+            foreach (var c in s)
+            {
+                charByNumber.Add((c, rowNumber));
+
+                if (isDown)
+                {
+                    if (rowNumber == numRows)
+                    {
+                        isDown = false;
+                        rowNumber--;
+                    }
+                    else
+                    {
+                        rowNumber++;
+                    }
+                }
+                else
+                {
+                    if (rowNumber == 1)
+                    {
+                        isDown = true;
+                        rowNumber++;
+                    }
+                    else
+                    {
+                        rowNumber--;
+                    }
+                }
+            }
+
+            return charByNumber;
+        }
+    }
+}
---
+++ LeetCode.Tests/P0006ZigZagConversionTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class P0006ZigZagConversionTests
+    {
+        private readonly P0006ZigZagConversion _sut = new();
+
+        [Fact]
+        public void Convert_StringLengthEqualNumberOfStrings_TheSame()
+        {
+            var seed = "PaypalIsHiring";
+            _sut.Convert(seed, seed.Length).Should().Be(seed);
+        }
+
+        [Fact]
+        public void Convert_OneSymbol_TheSame()
+        {
+            var seed = ".";
+            _sut.Convert(seed, seed.Length).Should().Be(seed);
+        }
+
+        [Theory]
+        [InlineData("PaypalIsHiring", 3, "PaHnaplsiigyIr")]
+        [InlineData("PaypalIsHiring", 4, "PInalsigyaHrpi")]
+        [InlineData("ab", 1, "ab")]
+        [InlineData("abcdefg", 2, "acegbdf")]
+        public void Convert_Sample_MatchExpectations(string seed, int rowNumber, string expected)
+        {
+            _sut.Convert(seed, rowNumber).Should().Be(expected);
+        }
+
+    }
+}
