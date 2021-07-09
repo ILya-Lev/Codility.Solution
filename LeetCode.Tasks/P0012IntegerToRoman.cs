9f194f31934b660ccfbb6c7031978fcc2d4103f8
Illia Levandovskyi <lis2889@gmail.com>
2021-07-09 19:29:34 +0300

    p0012 accepted, 144 ms 9.38% faster and 27.7 mb 7.29 % less memory

---
+++ LeetCode.Tasks/P0012IntegerToRoman.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Tasks
+{
+    public class P0012IntegerToRoman
+    {
+        private static readonly Dictionary<char, int> _mapR = new Dictionary<char, int>()
+        {
+            ['I'] = 1,
+            ['V'] = 5,
+            ['X'] = 10,
+            ['L'] = 50,
+            ['C'] = 100,
+            ['D'] = 500,
+            ['M'] = 1000,
+        };
+
+        private static readonly Dictionary<int, char> _map = _mapR
+            .OrderByDescending(p => p.Value)
+            .ToDictionary(p => p.Value, p => p.Key);
+
+        private static readonly IReadOnlyDictionary<int, int> _subtraction = new Dictionary<int, int>()
+        {
+            [1000] = 100,
+            [500] = 100,
+            [100] = 10,
+            [50] = 10,
+            [10] = 1,
+            [5] = 1,
+        };
+
+        public string IntToRoman(int num)
+        {
+            var roman = new List<char>();
+            foreach (var p in _map)
+            {
+                if (num <= 0) break;
+
+                for (int i = 0; i < num / p.Key; i++)
+                {
+                    roman.Add(p.Value);
+                }
+
+                num %= p.Key;
+                if (num <= 0) break;
+
+                var subtractingNumber = p.Key - _subtraction[p.Key];
+                if (num >= subtractingNumber)               //e.g. 92
+                {
+                    roman.Add(_map[_subtraction[p.Key]]);   //e.g. add X
+                    roman.Add(p.Value);                     //e.g. add C
+                    num %= subtractingNumber;               //e.g. 92 -> 92 % 90 -> 2
+                }
+            }
+
+            return new string(roman.ToArray());
+        }
+    }
+}
---
+++ LeetCode.Tests/P0012IntegerToRomanTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class P0012IntegerToRomanTests
+    {
+        private readonly P0012IntegerToRoman _sut = new();
+
+        [Theory]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(9, "IX")]
+        [InlineData(58, "LVIII")]
+        [InlineData(1994, "MCMXCIV")]
+        public void IntToRoman_Sample_AsExpected(int integer, string roman)
+        {
+            _sut.IntToRoman(integer).Should().Be(roman);
+        }
+    }
+}
