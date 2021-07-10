e283661dc334bb00d61e69468e3439a567e5cc42
Illia Levandovskyi <lis2889@gmail.com>
2021-07-10 13:25:52 +0300

    p0273 accepted, 144 ms 5.35% faster, 24.5 mb 60.29% less memory

---
+++ LeetCode.Tasks/P0273IntegerToEnglishWords.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Tasks
+{
+    public class P0273IntegerToEnglishWords
+    {
+        private static readonly IReadOnlyDictionary<int, string> _map = new Dictionary<int, string>()
+        {
+            [1_000_000_000] = "Billion",
+            [1_000_000] = "Million",
+            [1_000] = "Thousand",
+            [100] = "Hundred",
+
+            [90] = "Ninety",
+            [80] = "Eighty",
+            [70] = "Seventy",
+            [60] = "Sixty",
+            [50] = "Fifty",
+            [40] = "Forty",
+            [30] = "Thirty",
+            [20] = "Twenty",
+
+            [19] = "Nineteen",
+            [18] = "Eighteen",
+            [17] = "Seventeen",
+            [16] = "Sixteen",
+            [15] = "Fifteen",
+            [14] = "Fourteen",
+            [13] = "Thirteen",
+            [12] = "Twelve",
+            [11] = "Eleven",
+            [10] = "Ten",
+
+            [9] = "Nine",
+            [8] = "Eight",
+            [7] = "Seven",
+            [6] = "Six",
+            [5] = "Five",
+            [4] = "Four",
+            [3] = "Three",
+            [2] = "Two",
+            [1] = "One",
+            [0] = "Zero",
+        };
+
+        public string NumberToWords(int num)
+        {
+            if (num == 0) return _map[num];
+
+            var parts = GetParts(num);
+
+            return string.Join(" ", parts);
+        }
+
+        private static List<string> GetParts(int number)
+        {
+            if (number < 10) return new List<string>() { _map[number] };
+
+            var parts = new List<string>();
+            foreach (var p in _map)
+            {
+                if (number == 0) break;
+
+                var current = number / p.Key;
+                if (current != 0)
+                {
+                    if (p.Key >= 100)
+                        parts.AddRange(GetParts(current));
+
+                    parts.Add(p.Value);
+                }
+
+                number %= p.Key;
+            }
+
+            return parts;
+        }
+    }
+}
---
+++ LeetCode.Tests/P0273IntegerToEnglishWordsTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class P0273IntegerToEnglishWordsTests
+    {
+        private readonly P0273IntegerToEnglishWords _sut = new();
+
+        [Theory]
+        [InlineData(1234567891, "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One")]
+        [InlineData(1234567, "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven")]
+        [InlineData(12345, "Twelve Thousand Three Hundred Forty Five")]
+        [InlineData(123, "One Hundred Twenty Three")]
+        [InlineData(12, "Twelve")]
+        [InlineData(10, "Ten")]
+        [InlineData(1, "One")]
+        [InlineData(0, "Zero")]
+        public void NumberToWords_Sample_MatchExpectations(int n, string s)
+        {
+            _sut.NumberToWords(n).Should().Be(s);
+        }
+    }
+}
