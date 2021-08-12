edfc4021e38e64bf316b6e9be831527c43b35c0e
Illia Levandovskyi <lis2889@gmail.com>
2021-08-12 23:50:41 +0300

    1493 longest subarray after 1 item removed, 136 ms 78% faster, 35.4 mb 46% less

---
+++ LeetCode.Tasks/P1493LongestOnesSequence.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Tasks
+{
+    public class P1493LongestOnesSequence
+    {
+        public int LongestSubarray(int[] nums)
+        {
+            var aggregated = CollapseOnes(nums).ToArray();
+            if (aggregated.Length == 0) return nums.Length - 1;//there are 1s only
+
+            if (aggregated.Length == 1) return Math.Max(aggregated[0] - 1, 0);
+
+            if (aggregated.Length == 2) return Math.Max(aggregated[0], aggregated[1]);
+
+            var max = 0;
+            for (int i = 0; i < aggregated.Length - 2; i++)
+            {
+                max = Math.Max(max, aggregated[i] + aggregated[i + 1] + aggregated[i + 2]);
+            }
+
+            return max;
+        }
+
+        private static IEnumerable<int> CollapseOnes(int[] nums)
+        {
+            var currentSum = 0;
+            foreach (var n in nums)
+            {
+                if (n != 1)
+                {
+                    if (currentSum != 0)
+                    {
+                        yield return currentSum;
+                        currentSum = 0;
+                    }
+
+                    yield return 0;
+                }
+                else
+                {
+                    currentSum++;
+                }
+            }
+
+            yield return currentSum;
+        }
+    }
+}
---
+++ LeetCode.Tests/P1493LongestOnesSequenceTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using LeetCode.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class P1493LongestOnesSequenceTests
+    {
+        private readonly P1493LongestOnesSequence _sut = new();
+
+        [Fact]
+        public void LongestSubarray_OnlyZeros_0()
+        {
+            _sut.LongestSubarray(new[] { 0, 0, 0, 0 }).Should().Be(0);
+            _sut.LongestSubarray(new[] { 0, 0, 0 }).Should().Be(0);
+            _sut.LongestSubarray(new[] { 0, 0 }).Should().Be(0);
+            _sut.LongestSubarray(new[] { 0 }).Should().Be(0);
+        }
+
+        [Fact]
+        public void LongestSubarray_OnlyOnes_LenM1()
+        {
+            _sut.LongestSubarray(new[] { 1, 1, 1, 1 }).Should().Be(3);
+            _sut.LongestSubarray(new[] { 1, 1, 1 }).Should().Be(2);
+            _sut.LongestSubarray(new[] { 1, 1 }).Should().Be(1);
+            _sut.LongestSubarray(new[] { 1 }).Should().Be(0);
+        }
+
+        [Fact]
+        public void LongestSubarray_ZeroAtEnd_LengthButOne()
+        {
+            _sut.LongestSubarray(new[] { 0, 1, 1, 1 }).Should().Be(3);
+            _sut.LongestSubarray(new[] { 0, 1, 1 }).Should().Be(2);
+            _sut.LongestSubarray(new[] { 0, 1 }).Should().Be(1);
+        }
+
+        [Fact]
+        public void LongestSubarray_Zebra_One()
+        {
+            _sut.LongestSubarray(new[] { 0, 1, 0, 1 }).Should().Be(2);
+            _sut.LongestSubarray(new[] { 0, 1, 0 }).Should().Be(1);
+        }
+    }
+}
