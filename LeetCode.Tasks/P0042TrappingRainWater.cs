aca3f303921b419d3e763306d1fe186c22211f18
Illia Levandovskyi <lis2889@gmail.com>
2021-08-28 16:53:40 +0300

    P0042 accepted, 114ms 20% faster; 26.6MB 25% less

---
+++ LeetCode.Tasks/P0042TrappingRainWater.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LeetCode.Tasks
+{
+    public class P0042TrappingRainWater
+    {
+        public int Trap(int[] height)
+        {
+            return DoTrap(height, 0, height.Length - 1);
+        }
+
+        private int DoTrap(int[] height, int start, int end)
+        {
+            var water = 0;
+            while (start < end)
+            {
+                var current = 0;
+                if (height[start] <= height[end])
+                {
+                    for (current = start + 1; current < end; current++)
+                    {
+                        if (height[current] >= height[start])
+                            break;
+                    }
+                    water += GetWaterAmount(height, height[start], start + 1, current);
+                    start = current;
+                    continue;
+                }
+
+                //height[start] > height[end]
+                for (current = end - 1; current > start; current--)
+                {
+                    if (height[current] >= height[end])
+                        break;
+                }
+                water += GetWaterAmount(height, height[end], current + 1, end);
+                end = current;
+            }
+
+            return water;
+        }
+
+        private int GetWaterAmount(int[] height, int edgeHeight, int start, int endExclusively)
+        {
+            var water = 0;
+            for (int i = start; i < endExclusively; i++)
+            {
+                water += edgeHeight - height[i];
+            }
+            return water;
+        }
+    }
+}
---
+++ LeetCode.Tests/P0042TrappingRainWaterTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using FluentAssertions;
+using LeetCode.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    [Trait("Category", "Unit")]
+    public class P0042TrappingRainWaterTests
+    {
+        private readonly P0042TrappingRainWater _sut = new();
+
+        [Fact]
+        public void Trap_Sample1_6()
+        {
+            var height = new[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            _sut.Trap(height).Should().Be(6);
+        }
+
+        [Fact]
+        public void Trap_Hill_Zero()
+        {
+            int m = 100_000;
+            var height = Enumerable.Range(1, m).Concat(Enumerable.Range(1, m).Reverse()).ToArray();
+            _sut.Trap(height).Should().Be(0);
+        }
+
+        [Fact]
+        public void Trap_Valley_Zero()
+        {
+            int m = 10_000;
+            var height = Enumerable.Range(1, m).Reverse().Concat(Enumerable.Range(1, m)).ToArray();
+            _sut.Trap(height).Should().Be(height.Sum(n => m-n));
+        }
+    }
+}
