18c0f4e5580204bf9e7b6bea00380e82b3a7b798
Illia Levandovskyi <lis2889@gmail.com>
2021-08-28 16:18:57 +0300

    p0011 accepted; 265ms 18.33% faster; 44.1 MB 92.22% smaller

--- LeetCode.Tasks/P0011MostWater.cs
+++ LeetCode.Tasks/P0011MostWater.cs
@@ -8,70 +8,62 @@ namespace LeetCode.Tasks
 {
     public class P0011MostWater
     {
-        /// <summary> O(n^2) - too slow </summary>
-        public int NaiveMaxArea(int[] height)
+        public class Rectangle
         {
-            var max = 0;
+            public int FirstIndex { get; set; }
+            public int SecondIndex { get; set; }
+            public int FirstHeight { get; set; }
+            public int SecondHeight { get; set; }
+            public int Area => Math.Min(FirstHeight, SecondHeight) * (SecondIndex - FirstIndex);
+        }
+        /// <summary> O(n^2) - too slow </summary>
+        public Rectangle NaiveMaxArea(int[] height)
+        {
+            var max = new Rectangle();
             for (int i = 0; i < height.Length; i++)
             {
                 for (int j = i + 1; j < height.Length; j++)
                 {
                     var currentArea = Math.Min(height[j], height[i]) * (j - i);
-                    max = Math.Max(currentArea, max);
+                    if (currentArea > max.Area)
+                    {
+                        max.FirstIndex = i;
+                        max.SecondIndex = j;
+                        max.FirstHeight = height[i];
+                        max.SecondHeight = height[j];
+                    }
                 }
             }
             return max;
         }
 
         /// <summary>
-        /// still too slow - for natural numbers sequence ...
+        /// I believe may lead to wrong answers as in case of collision only front is moved
         /// </summary>
         public int MaxArea(int[] height) => GetMaxArea(height, 0, height.Length-1);
 
         private int GetMaxArea(int[] height, int start, int end)
         {
-            var currentArea = GetArea(height, start, end);
+            var currentArea = 0;
             while (start < end)
             {
-                if (height[start] < height[end])
-                    start = MoveStart(height, start, end);
-                else if (height[start] > height[end])
-                    end = MoveEnd(height, start, end);
-                else 
-                    break;
-
                 currentArea = Math.Max(currentArea, GetArea(height, start, end));
+                if (height[start] <= height[end])
+                {
+                    start++;
+                    continue;
+                }
+
+                if (height[start] > height[end])
+                {
+                    end--;
+                    continue;
+                }
             }
 
-            if (start < end)
-                return Math.Max(currentArea
-                                ,Math.Max(GetMaxArea(height, start + 1, end)
-                                        , GetMaxArea(height, start, end - 1))
-                );
+            currentArea = Math.Max(currentArea, GetArea(height, start, end));
 
             return currentArea;
-        }
-
-        private int MoveEnd(int[] heights, int start, int end)
-        {
-            for (int i = end - 1; i > start; i--)
-            {
-                if (heights[i] > heights[end])
-                    return i;
-            }
-
-            return start;
-        }
-
-        private int MoveStart(int[] heights, int start, int end)
-        {
-            for (int i = start+1; i < end; i++)
-            {
-                if (heights[i] > heights[start])
-                    return i;
-            }
-
-            return end;
         }
 
         private int GetArea(int[] heights, int start, int end) => 
---
+++ LeetCode.Tests/P0011MostWaterTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using FluentAssertions;
+using LeetCode.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.Tests
+{
+    [Trait("Category", "Unit")]
+    public class P0011MostWaterTests
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly P0011MostWater _sut = new();
+
+        public P0011MostWaterTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void MaxArea_Small1_MatchNaive()
+        {
+            var heights = new[] { 1, 8, 4, 3, 5, 8, 7 };//8x4 or 7x5
+            
+            _sut.MaxArea(heights).Should().Be(35);
+            
+            var naiveMaxArea = _sut.NaiveMaxArea(heights);
+            naiveMaxArea.Area.Should().Be(35);
+
+            _output.WriteLine(JsonConvert.SerializeObject(naiveMaxArea, Formatting.Indented));
+
+        }
+
+        [Fact]
+        public void MaxArea_10Pow4Straight_MatchNaive()
+        {
+            var heights = Enumerable.Range(1, 10_000).ToArray();
+
+            var stopwatchNaive = Stopwatch.StartNew();
+            var naiveMaxArea = _sut.NaiveMaxArea(heights);
+            stopwatchNaive.Stop();
+
+            var stopwatch = Stopwatch.StartNew();
+            var maxArea = _sut.MaxArea(heights);
+            stopwatch.Stop();
+
+            maxArea.Should().Be(naiveMaxArea.Area);
+            stopwatch.ElapsedMilliseconds.Should().BeLessThan(stopwatchNaive.ElapsedMilliseconds);
+            _output.WriteLine($"max area = {maxArea:### ### ###};" +
+                              $" naive approach took {stopwatchNaive.ElapsedMilliseconds} ms;" +
+                              $" smarter approach took {stopwatch.ElapsedMilliseconds}");
+            
+            _output.WriteLine(JsonConvert.SerializeObject(naiveMaxArea, Formatting.Indented));
+        }
+
+        [Fact]
+        public void MaxArea_10Pow4Hill_MatchNaive()
+        {
+            var m = 10_000;//middle
+            var heights = Enumerable.Range(1, m).Concat(Enumerable.Range(1, m).Reverse()).ToArray();
+
+            var stopwatchNaive = Stopwatch.StartNew();
+            var naiveMaxArea = _sut.NaiveMaxArea(heights);
+            stopwatchNaive.Stop();
+
+            var stopwatch = Stopwatch.StartNew();
+            var maxArea = _sut.MaxArea(heights);
+            stopwatch.Stop();
+
+            maxArea.Should().Be(naiveMaxArea.Area);
+            stopwatch.ElapsedMilliseconds.Should().BeLessThan(stopwatchNaive.ElapsedMilliseconds);
+            _output.WriteLine($"max area = {maxArea:### ### ###};" +
+                              $" naive approach took {stopwatchNaive.ElapsedMilliseconds} ms;" +
+                              $" smarter approach took {stopwatch.ElapsedMilliseconds}");
+
+            _output.WriteLine(JsonConvert.SerializeObject(naiveMaxArea, Formatting.Indented));
+        }
+    }
+}
