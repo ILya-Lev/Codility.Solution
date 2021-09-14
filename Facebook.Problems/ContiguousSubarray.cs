4c0444d54d901803746217fccd87b233ac73c68f
Illia Levandovskyi <lis2889@gmail.com>
2021-09-14 16:23:21 +0300

    contiguous subarray problem naive impl

---
+++ Facebook.Problems/ContiguousSubarray.cs
@@ -0,0 +1,41 @@
+﻿namespace Facebook.Problems
+{
+    public class ContiguousSubarray
+    {
+        //is it possible to optimize given solution with a Trie or a Heap data structure?
+        public static int[] CountSubarrays(int[] arr)
+        {
+            // Write your code here
+
+            var subarrayAmount = new int[arr.Length]; //time: O(N^2); memory: O(N)
+            for (int i = 0; i < arr.Length; i++)
+            {
+                subarrayAmount[i] = 1
+                                    + LeftSubarrayAmount(arr, i)  //to make the overall algorithm faster
+                                    + RightSubarrayAmount(arr, i);//make L/R sub array methods faster than O(N)
+            }
+
+            return subarrayAmount;
+        }
+
+        private static int LeftSubarrayAmount(int[] arr, int currentPosition)
+        {
+            for (int i = currentPosition - 1; i >= 0; i--)
+            {
+                if (arr[i] > arr[currentPosition])
+                    return currentPosition - i - 1;
+            }
+            return currentPosition;
+        }
+
+        private static int RightSubarrayAmount(int[] arr, int currentPosition)
+        {
+            for (int i = currentPosition + 1; i < arr.Length; i++)
+            {
+                if (arr[i] > arr[currentPosition])
+                    return i - currentPosition - 1;
+            }
+            return arr.Length - currentPosition - 1;
+        }
+    }
+}
---
+++ Facebook.Tests/ContiguousSubarrayTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Facebook.Problems;
+using FluentAssertions;
+using Xunit;
+
+namespace Facebook.Tests
+{
+    public class ContiguousSubarrayTests
+    {
+        [Fact]
+        public void CountSubarrays_Sample1_ExpectedResult()
+        {
+            ContiguousSubarray.CountSubarrays(new[] { 3, 4, 1, 6, 2 })
+                .Should().Equal(new []{1,3,1,5,1});
+        }
+
+        [Fact]
+        public void CountSubarrays_NaturalNumbers_TheSame()
+        {
+            var numbers = Enumerable.Range(1, 10).ToArray();
+            
+            ContiguousSubarray.CountSubarrays(numbers)
+                .Should().Equal(numbers);
+        }
+
+        [Fact]
+        public void CountSubarrays_ReverseNaturalNumbers_TheSame()
+        {
+            var numbers = Enumerable.Range(1, 10_000).Reverse().ToArray();
+            
+            ContiguousSubarray.CountSubarrays(numbers)
+                .Should().Equal(numbers);
+        }
+    }
+}
