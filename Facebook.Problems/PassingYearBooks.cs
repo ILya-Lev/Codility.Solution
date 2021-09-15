326d58fb1daa21d69c6356e78cf69283b451f876
Illia Levandovskyi <lis2889@gmail.com>
2021-09-15 20:19:28 +0300

    passing yearbooks

---
+++ Facebook.Problems/PassingYearBooks.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Facebook.Problems
+{
+    public class PassingYearBooks
+    {
+        private class Book
+        {
+            public int Owner { get; }
+            public int Location { get; set; }
+            public int SignedTimes { get; set; }
+            public Book(int owner)
+            {
+                Owner = owner;
+                Location = owner;
+            }
+        }
+
+        public static int[] FindSignatureCounts(int[] arr)
+        {
+            var booksToMove = Enumerable.Range(1, arr.Length).Select(owner => new Book(owner)).ToList();//ideally linked list
+            var completelySignedBooks = new List<Book>(arr.Length);
+
+            while (booksToMove.Any())
+            {
+                SignBooks(booksToMove);
+
+                MoveBooks(arr, booksToMove, completelySignedBooks);
+            }
+
+            return completelySignedBooks.OrderBy(b => b.Owner).Select(b => b.SignedTimes).ToArray();
+        }
+
+        private static void SignBooks(List<Book> booksToMove) => booksToMove.ForEach(b => b.SignedTimes++);
+
+        private static void MoveBooks(int[] arr, List<Book> booksToMove, List<Book> completelySignedBooks)
+        {
+            for (int c = 0; c < booksToMove.Count; c++)
+            {
+                var currentBook = booksToMove[c];
+                if (arr[currentBook.Location - 1] == currentBook.Owner)
+                {
+                    booksToMove.RemoveAt(c--);
+                    completelySignedBooks.Add(currentBook);
+                    continue;
+                }
+
+                currentBook.Location = arr[currentBook.Location - 1];
+            }
+        }
+    }
+}
---
+++ Facebook.Tests/PassingYearBooksTests.cs
@@ -0,0 +1,28 @@
+﻿using Facebook.Problems;
+using FluentAssertions;
+using Xunit;
+
+namespace Facebook.Tests
+{
+    public class PassingYearBooksTests
+    {
+        [Fact]
+        public void FindSignatureCounts_Sample1_All2()
+        {
+            PassingYearBooks.FindSignatureCounts(new[] { 2, 1 }).Should().Equal(new[] { 2, 2 });
+        }
+
+        [Fact]
+        public void FindSignatureCounts_Sample2_All1()
+        {
+            PassingYearBooks.FindSignatureCounts(new[] { 1, 2 }).Should().Equal(new[] { 1, 1 });
+        }
+
+        [Fact]
+        public void FindSignatureCounts_Sample3_Ok()
+        {
+            PassingYearBooks.FindSignatureCounts(new[] {3, 2, 1 })
+                .Should().Equal(new[] { 2, 1, 2 });
+        }
+    }
+}
