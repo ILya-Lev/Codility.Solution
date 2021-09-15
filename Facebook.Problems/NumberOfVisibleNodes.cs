fe4c85c8f2587d3105f2945573d2677a1fd03345
Illia Levandovskyi <lis2889@gmail.com>
2021-09-16 00:50:25 +0300

    number of visible nodes

---
+++ Facebook.Problems/NumberOfVisibleNodes.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook.Problems
+{
+    public class NumberOfVisibleNodes
+    {
+        public class Node
+        {
+            public int Value { get; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+
+            public Node(int value) => Value = value;
+        }
+
+        public static int VisibleNodes(Node root)
+        {
+            var level = new Queue<Node>();
+            level.Enqueue(root);
+
+            var maxDepth = 0;
+            while (level.Any())
+            {
+                level.Enqueue(null);//current level is completely traversed token
+                maxDepth++;
+                for (var current = level.Dequeue(); current != null; current = level.Dequeue())
+                {
+                    if (current.Left != null) level.Enqueue(current.Left);
+                    if (current.Right != null) level.Enqueue(current.Right);
+                }
+            }
+            return maxDepth;
+        }
+
+        public static int VisibleNodesByDepth(Node root)
+        {
+            var depth = new Stack<Node>();
+            depth.Push(root);
+
+            var maxDepth = 0;
+            while (depth.Any())
+            {
+                var current = depth.Peek();
+                if (current.Left != null)
+                {
+                    depth.Push(current.Left);
+                    current.Left = null;
+                    continue;
+                }
+
+                if (current.Right != null)
+                {
+                    depth.Push(current.Right);
+                    current.Right = null;
+                    continue;
+                }
+
+                maxDepth = Math.Max(maxDepth, depth.Count);
+                depth.Pop();
+            }
+            return maxDepth;
+        }
+
+        public static List<int> InAscendingOrder(Node root)
+        {
+            var sorted = new List<int>();
+
+            var stack = new Stack<Node>();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Peek();
+                if (current.Left != null)
+                {
+                    stack.Push(current.Left);
+                    current.Left = null;
+                    continue;
+                }
+                
+                sorted.Add(current.Value);
+                stack.Pop();
+
+                if (current.Right != null)
+                    stack.Push(current.Right);
+            }
+
+            return sorted;
+        }
+
+        private static List<int> _ascending = new List<int>();
+        public static List<int> InAscendingOrderRecursion(Node root)
+        {
+            _ascending.Clear();
+
+            if (root != null) FillIn(root);
+
+            return _ascending;
+        }
+
+        private static void FillIn(Node root)
+        {
+            if (root.Left != null) FillIn(root.Left);
+            
+            _ascending.Add(root.Value);
+            
+            if (root.Right != null) FillIn(root.Right);
+        }
+    }
+}
---
+++ Facebook.Tests/NumberOfVisibleNodesTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Facebook.Problems;
+using FluentAssertions;
+using Xunit;
+
+namespace Facebook.Tests
+{
+    public class NumberOfVisibleNodesTests
+    {
+        private readonly NumberOfVisibleNodes.Node _root = new (8)
+        {
+            Left = new NumberOfVisibleNodes.Node(3)
+            {
+                Left = new NumberOfVisibleNodes.Node(1),
+                Right = new NumberOfVisibleNodes.Node(6)
+                {
+                    Left = new NumberOfVisibleNodes.Node(4),
+                    Right = new NumberOfVisibleNodes.Node(7),
+                }
+            },
+            Right = new NumberOfVisibleNodes.Node(10)
+            {
+                Right = new NumberOfVisibleNodes.Node(14)
+                {
+                    Left = new NumberOfVisibleNodes.Node(13)
+                }
+            }
+        };
+
+
+        [Fact]
+        public void VisibleNodes_Sample_4()
+        {
+            NumberOfVisibleNodes.VisibleNodes(_root).Should().Be(4);
+        }
+
+        [Fact]
+        public void VisibleNodesByDepth_Sample_4()
+        {
+            NumberOfVisibleNodes.VisibleNodesByDepth(_root).Should().Be(4);
+        }
+
+        [Fact]
+        public void InAscendingOrder_Sample_Ok()
+        {
+            NumberOfVisibleNodes.InAscendingOrder(_root)
+                .Should().Equal(new[] { 1, 3, 4, 6, 7, 8, 10, 13, 14 });
+        }
+
+        [Fact]
+        public void InAscendingOrderRecursion_Sample_Ok()
+        {
+            NumberOfVisibleNodes.InAscendingOrderRecursion(_root)
+                .Should().Equal(new[] { 1, 3, 4, 6, 7, 8, 10, 13, 14 });
+        }
+    }
+}
