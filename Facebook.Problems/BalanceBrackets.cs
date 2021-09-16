447189bf68683e51f8309a4d70b977f8f9bfbac5
Illia Levandovskyi <lis2889@gmail.com>
2021-09-16 19:54:32 +0300

    balance brackets

---
+++ Facebook.Problems/BalanceBrackets.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Facebook.Problems
+{
+    public class BalanceBrackets
+    {
+        private static readonly HashSet<char> _openingBrackets = new HashSet<char>()
+        {
+            '(', '[', '{'
+        };
+
+        private static readonly IReadOnlyDictionary<char, char> _bracketPairs = new Dictionary<char, char>()
+        {
+            [')'] = '(',
+            ['}'] = '{',
+            [']'] = '[',
+        };
+
+        public static bool IsBalanced(string s)
+        {
+            // Write your code here
+            var notMatched = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (_openingBrackets.Contains(c))
+                {
+                    notMatched.Push(c);
+                    continue;
+                }
+
+                if (!notMatched.Any()) return false;
+
+                var mostRecentOpeningBracket = notMatched.Pop();
+                if (_bracketPairs[c] != mostRecentOpeningBracket) return false;
+            }
+
+            return notMatched.Count == 0;
+        }
+    }
+}
---
+++ Facebook.Tests/BalanceBracketsTests.cs
@@ -0,0 +1,19 @@
+﻿using Facebook.Problems;
+using FluentAssertions;
+using Xunit;
+
+namespace Facebook.Tests
+{
+    public class BalanceBracketsTests
+    {
+        [Theory]
+        [InlineData("{[()]}", true)]
+        [InlineData("{}[]", true)]
+        [InlineData("{[}]", false)]
+        [InlineData(")", false)]
+        public void IsBalanced_Example_MatchExpectations(string input, bool expectations)
+        {
+            BalanceBrackets.IsBalanced(input).Should().Be(expectations);
+        }
+    }
+}
