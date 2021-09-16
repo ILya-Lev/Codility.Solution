b0dd73f9d9544f6444985f26e3d722f0a5dae225
Illia Levandovskyi <lis2889@gmail.com>
2021-09-16 17:49:56 +0300

    min len substr - won't work for duplicates in string t

---
+++ Facebook.Problems/MinimumLengthSubstrings.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Facebook.Problems
+{
+    public class MinimumLengthSubstrings
+    {
+        public static int MinLengthSubstring(string s, string t)
+        {
+            // Write your code here
+            var frequencyTable = ComposeFrequencyTable(s, t).ToArray();
+            if (frequencyTable.Length != t.Length) return -1;
+
+            var rangeWidth = FindTheMostNarrowRange(frequencyTable);
+
+            return rangeWidth;
+        }
+
+        private static IEnumerable<List<int>> ComposeFrequencyTable(string s, string t)
+        {
+            var sFrequencies = s
+              .Select((c, idx) => (c, idx))
+              .GroupBy(e => e.c, e => e.idx)
+              .ToDictionary(g => g.Key, g => g.ToList());
+
+            foreach (var c in t)
+            {
+                if (!sFrequencies.ContainsKey(c))
+                    yield break;
+                yield return sFrequencies[c];
+            }
+        }
+
+        private static int FindTheMostNarrowRange(List<int>[] ft)
+        {
+            var minWidth = int.MaxValue;
+            var allIndexes = ft.SelectMany(c => c).OrderBy(idx => idx).ToArray();//O(N*lgN)
+
+            foreach (var firstIndex in allIndexes)
+            {//O(N^3/2*lgN*lgN)
+                int min = firstIndex, max = firstIndex;
+
+                var columnToIgnore = FindColumnToIgnore(ft, firstIndex);
+
+                for (int i = 0; i < ft.Length; i++)
+                {
+                    if (i == columnToIgnore)
+                        continue;
+
+                    var closest = FindTheClosest(firstIndex, ft[i]);
+                    min = Math.Min(min, closest);
+                    max = Math.Max(max, closest);
+                }
+
+                minWidth = Math.Min(minWidth, max - min + 1);
+            }
+
+            return minWidth;
+        }
+
+        //O(N^1/2*lgN)
+        private static int FindColumnToIgnore(List<int>[] ft, int firstIndex)
+        {
+            for (int i = 0; i < ft.Length; i++)
+            {
+                if (ft[i].BinarySearch(firstIndex) >= 0)
+                    return i;
+            }
+            throw new Exception($"Cannot find a column with {firstIndex} in it.");
+        }
+
+        private static int FindTheClosest(int seed, List<int> indexes)
+        {
+            var minDistance = int.MaxValue;
+            foreach (var idx in indexes)
+            {
+                var distance = idx - seed;
+                if (distance == 0)
+                    continue;
+                minDistance = Math.Min(minDistance, distance);
+            }
+            return minDistance + seed;
+        }
+    }
+}
---
+++ Facebook.Tests/MinimumLengthSubstringsTests.cs
@@ -0,0 +1,22 @@
+﻿using Facebook.Problems;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using Xunit;
+
+namespace Facebook.Tests
+{
+    public class MinimumLengthSubstringsTests
+    {
+        [Fact]
+        public void MinLengthSubstring_Sample1_5()
+        {
+            MinimumLengthSubstrings.MinLengthSubstring("dcbefebce", "fd").Should().Be(5);
+        }
+
+        [Fact]
+        public void MinLengthSubstring_Sample2_5()
+        {
+            MinimumLengthSubstrings.MinLengthSubstring("dcbeffffebce", "fd").Should().Be(5);
+        }
+    }
+}
