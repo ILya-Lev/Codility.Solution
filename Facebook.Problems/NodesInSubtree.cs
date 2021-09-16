c090950b90ad80e423af2182479440764c52f796
Illia Levandovskyi <lis2889@gmail.com>
2021-09-16 18:54:00 +0300

    node in subtree task - another one with redundant info in the task description, suspiciously too easy

---
+++ Facebook.Problems/NodesInSubtree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Facebook.Problems
+{
+    public class NodesInSubtree
+    {
+        public class Node
+        {
+            public int U { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        public class Query
+        {
+            public int U { get; set; }
+            public char C { get; set; }
+        }
+
+        public static int[] CountOfNodes(Node root, List<Query> queries, String s)
+        {
+            var subtreePopulation = new Dictionary<int, int>();
+            GetCurrentPopulation(root, subtreePopulation);
+
+            return queries.Select(q => subtreePopulation[q.U]).ToArray();
+        }
+
+        private static int GetCurrentPopulation(Node root, Dictionary<int, int> population)
+        {
+            if (root is null) return 0;
+
+            var subPopulation = GetCurrentPopulation(root.Left, population)
+                                + GetCurrentPopulation(root.Right, population);
+
+            population.Add(root.U, subPopulation);
+
+            return subPopulation + 1;
+        }
+    }
+}
---
+++ Facebook.Tests/NodesInSubtreeTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Facebook.Problems;
+using FluentAssertions;
+using Xunit;
+
+namespace Facebook.Tests
+{
+    public class NodesInSubtreeTests
+    {
+        [Fact]
+        public void CountOfNodes_Sample1_2()
+        {
+            var root = new NodesInSubtree.Node()
+            {
+                U = 1,
+                Left = new NodesInSubtree.Node() { U = 2 },
+                Right = new NodesInSubtree.Node() { U = 3 },
+            };
+
+            var queries = new List<NodesInSubtree.Query>()
+            {
+                new NodesInSubtree.Query() { U = 1, C = 'a' }
+            };
+
+            NodesInSubtree.CountOfNodes(root, queries, null).Should().Equal(new[] { 2 });
+        }
+    }
+}
