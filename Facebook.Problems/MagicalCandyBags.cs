6895d08a92fb335fd80ec312cc127d2a92e04f2d
Illia Levandovskyi <lis2889@gmail.com>
2021-09-19 19:35:12 +0300

    magical candy bag problem solution is added

---
+++ Facebook.Problems/MagicalCandyBags.cs
@@ -0,0 +1,35 @@
+﻿using Algorithms.Solutions;
+
+namespace Facebook.Problems
+{
+    /*
+     You have N bags of candy. The ith bag contains arr[i] pieces of candy, and each of the bags is magical!
+It takes you 1 minute to eat all of the pieces of candy in a bag (irrespective of how many pieces of candy are inside), and as soon as you finish, the bag mysteriously refills. If there were x pieces of candy in the bag at the beginning of the minute, then after you've finished you'll find that floor(x/2) pieces are now inside.
+You have k minutes to eat as much candy as possible. How many pieces of candy can you eat?
+     */
+    public class MagicalCandyBags
+    {
+        /// <summary>
+        /// heapify => O(N), where N is arr.Length
+        /// loop => [k or sum(ceiling log by 2 of arr[i])] * log N => O(N*logN)
+        /// overall time complexity is O(N*logN)
+        /// space O(N)
+        /// </summary>
+        public static int MaxCandies(int[] arr, int k)
+        {
+            var maxHeap = MaxHeap<int>.Heapify(arr);
+            var remainingTime = k;
+            var total = 0;
+            while (remainingTime-- > 0 && maxHeap.Count > 0)
+            {
+                total += maxHeap.Head;
+
+                var refilled = maxHeap.Extract() / 2;
+                if (refilled > 0)
+                    maxHeap.Insert(refilled);
+            }
+
+            return total;
+        }
+    }
+}
---
+++ Facebook.Tests/MagicalCandyBagsTests.cs
@@ -0,0 +1,22 @@
+﻿using Facebook.Problems;
+using FluentAssertions;
+using Xunit;
+
+namespace Facebook.Tests
+{
+    [Trait("Category", "Unit")]
+    public class MagicalCandyBagsTests
+    {
+        [Fact]
+        public void MaxCandies_Sample1_14()
+        {
+            MagicalCandyBags.MaxCandies(new[] { 2, 1, 7, 4, 2 }, 3).Should().Be(14);
+        }
+
+        [Fact]
+        public void MaxCandies_SoMuchTimeThatBagsAreNotRefilledAnyMore_25()
+        {
+            MagicalCandyBags.MaxCandies(new[] { 2, 1, 7, 4, 2 }, 11).Should().Be(25);
+        }
+    }
+}
