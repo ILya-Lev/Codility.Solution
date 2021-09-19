743e8448c37112b008ca914b810b8dd802bcc512
Illia Levandovskyi <lis2889@gmail.com>
2021-09-19 18:32:35 +0300

    a few tests for largest triple product are added

--- Facebook.Tests/LargestTripleProductsTests.cs
+++ Facebook.Tests/LargestTripleProductsTests.cs
@@ -1,4 +1,5 @@
-﻿using Facebook.Problems;
+﻿using System;
+using Facebook.Problems;
 using FluentAssertions;
 using Xunit;
 
@@ -12,6 +13,33 @@ namespace Facebook.Tests
         {
             LargestTripleProducts.FindMaxProduct(new[] { 1, 2, 3, 4, 5 })
                 .Should().Equal(new[] { -1, -1, 6, 24, 60 });
+        }
+
+        [Fact]
+        public void FindMaxProduct_Empty_Empty()
+        {
+            LargestTripleProducts.FindMaxProduct(Array.Empty<int>()).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void FindMaxProduct_OneItem_minus1()
+        {
+            LargestTripleProducts.FindMaxProduct(new[] { 91_276 })
+                .Should().Equal(new[] { -1 });
+        }
+
+        [Fact]
+        public void FindMaxProduct_TwoItems_minus1()
+        {
+            LargestTripleProducts.FindMaxProduct(new[] { 91_276, 876_213 })
+                .Should().Equal(new[] { -1, -1 });
+        }
+
+        [Fact]
+        public void FindMaxProduct_TreeItems_MultiplyAll()
+        {
+            LargestTripleProducts.FindMaxProduct(new[] { 3, 5, 7 })
+                .Should().Equal(new[] { -1, -1, 105 });
         }
     }
 }
