cd43bb605e6b47ce579cba167570fd8ed3cba805
Illia Levandovskyi <lis2889@gmail.com>
2021-09-21 21:06:23 +0300

    dijkstra first attempt

--- Algorithms.Solutions/DijkstraShortestPath.cs
+++ Algorithms.Solutions/DijkstraShortestPath.cs
@@ -1,10 +1,56 @@
-﻿namespace Algorithms.Solutions
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Solutions
 {
     /// <summary>
     /// min path (by length of each edge) between the two given nodes
     /// </summary>
     public class DijkstraShortestPath
     {
+        public class Vertex
+        {
+            public int Data { get; set; }
+            
+            public List<Edge> Edges { get; } = new List<Edge>();
+        }
 
+        public class Edge : IComparable<Edge>
+        {
+            public int Length { get; set; }
+            public Vertex End { get; set; }
+
+            public int CompareTo(Edge other) => Length.CompareTo(other.Length);
+        }
+
+        /// <summary>
+        /// not correct so far... think it over!
+        /// </summary>
+        /// <param name="start">vertex to start the path from</param>
+        /// <param name="end">vertex to finish the path at</param>
+        /// <returns>vertex and a length of an edge pointing to it which is in the path</returns>
+        public Dictionary<Vertex, int> FindShortestPath(Vertex start, Vertex end)
+        {
+            var path = new Dictionary<Vertex, int> { { start, 0 } };
+
+            var superVertex = new HashSet<Vertex> { start };
+            var boundaryEdges = MinHeap<Edge>.Heapify(start.Edges);
+
+            while (!path.ContainsKey(end))
+            {
+                var shortestExternal = boundaryEdges.Extract();
+                if (!superVertex.Contains(shortestExternal.End))
+                {
+                    path.Add(shortestExternal.End, shortestExternal.Length);
+                    superVertex.Add(shortestExternal.End);
+                    foreach (var edge in shortestExternal.End.Edges)
+                    {
+                        if (!superVertex.Contains(edge.End))
+                            boundaryEdges.Insert(edge);
+                    }
+                }
+            }
+            return path;
+        }
     }
 }
