e7913184c94142d008b0c09416d198adbfe6c9a3
Illia Levandovskyi <lis2889@gmail.com>
2021-09-22 20:52:37 +0300

    mst prim's algorithm with heaps

---
+++ Algorithms.Solutions/PrimMinimumSpanningTree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Solutions
+{
+    public class PrimMinimumSpanningTree
+    {
+        public class Vertex<T>
+        {
+            public T Data { get; }
+            public List<Edge<T>> Edges { get; } = new List<Edge<T>>();
+
+            public Vertex(T data) => Data = data;
+        }
+
+        public class Edge<T> : IComparable<Edge<T>>
+        {
+            public int Length { get; }
+            public Vertex<T> End { get; set; }
+
+            public Edge(int length) => Length = length;
+            
+            public int CompareTo(Edge<T> other) => Length.CompareTo(other.Length);
+        }
+
+        public class SpanningTree<T>
+        {
+            private readonly HashSet<Vertex<T>> _vertices = new HashSet<Vertex<T>>();
+            private readonly List<Edge<T>> _edges = new List<Edge<T>>();
+
+            public IReadOnlyCollection<Vertex<T>> Vertices => _vertices;
+            public IReadOnlyCollection<Edge<T>> Edges => _edges;
+
+            public bool ContainsVertex(Vertex<T> vertex) => _vertices.Contains(vertex);
+            public void AddVertex(Vertex<T> vertex) => _vertices.Add(vertex);
+
+            public void AddEdge(Edge<T> edge) => _edges.Add(edge);
+        }
+
+        public static SpanningTree<T> DiscoverMst<T>(Vertex<T> seed)
+        {
+            var tree = new SpanningTree<T>();
+            tree.AddVertex(seed);
+
+            var externalEdges = MinHeap<Edge<T>>.Heapify(seed.Edges);
+
+            while (externalEdges.Count > 0)
+            {
+                var shortestEdge = externalEdges.Extract();
+                
+                var nextMember = shortestEdge.End;
+                if (tree.ContainsVertex(nextMember))
+                    continue;
+
+                tree.AddVertex(nextMember);
+                tree.AddEdge(shortestEdge);
+
+                foreach (var edge in nextMember.Edges.Where(e => !tree.ContainsVertex(e.End)))
+                {
+                    externalEdges.Insert(edge);
+                }
+            }
+
+            return tree;
+        }
+    }
+}
---
+++ Algorithms.Tests/PrimMinimumSpanningTreeTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Formats.Asn1;
+using System.Linq;
+using Algorithms.Solutions;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class PrimMinimumSpanningTreeTests
+    {
+        [Fact]
+        public void DiscoverMst_Sample1_Shortest()
+        {
+            #region setup
+            var a = new PrimMinimumSpanningTree.Vertex<char>('a');
+            var b = new PrimMinimumSpanningTree.Vertex<char>('b');
+            var c = new PrimMinimumSpanningTree.Vertex<char>('c');
+            var d = new PrimMinimumSpanningTree.Vertex<char>('d');
+
+            var ab = new PrimMinimumSpanningTree.Edge<char>(1) { End = b };
+            var bc = new PrimMinimumSpanningTree.Edge<char>(2) { End = c };
+            var ac = new PrimMinimumSpanningTree.Edge<char>(3) { End = c };
+            var cd = new PrimMinimumSpanningTree.Edge<char>(4) { End = d };
+            var ad = new PrimMinimumSpanningTree.Edge<char>(5) { End = d };
+
+            a.Edges.AddRange(new[] { ab, ac, ad });
+            b.Edges.Add(bc);
+            c.Edges.Add(cd);
+            #endregion setup
+
+            var tree = PrimMinimumSpanningTree.DiscoverMst(a);
+
+            using var scope = new AssertionScope();
+            tree.Edges.Sum(e => e.Length).Should().Be(7);
+            tree.Vertices.Should().HaveCount(4);
+        }
+    }
+}
