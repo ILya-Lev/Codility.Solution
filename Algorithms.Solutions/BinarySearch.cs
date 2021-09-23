9e03952d8baf936a7c2ee2ebd9012fbc6ca3b92c
Illia Levandovskyi <lis2889@gmail.com>
2021-09-23 20:46:42 +0300

    binary search is implemented

---
+++ Algorithms.Solutions/BinarySearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Solutions
+{
+    public class BinarySearch<T> where T : IComparable<T>
+    {
+        /// <summary>
+        /// Find a position for the key
+        /// </summary>
+        /// <param name="source">is expected to be sorted in ascending order</param>
+        /// <param name="key">item to find position in the <see cref="source"/> </param>
+        /// <returns>index it which <see cref="key"/> is found
+        /// or negative value, if ~ operation is applied to it, gives a position where missing
+        /// key should be inserted to maintain ascending sorted order</returns>
+        public static int Find(IReadOnlyList<T> source, T key)
+        {
+            int start = 0, end = source.Count-1;
+
+            var challengeStart = source[start].CompareTo(key);
+            if (challengeStart > 0) return ~start;
+            if (challengeStart == 0) return start;
+
+            var challengeEnd = source[end].CompareTo(key);
+            if (challengeEnd < 0) return ~end;
+            if (challengeEnd == 0) return end;
+
+            while (start <= end)
+            {
+                var middle = (start + end) / 2;
+                var challengeMiddle = source[middle].CompareTo(key);
+                
+                if (challengeMiddle == 0)
+                    return middle;
+                
+                if (challengeMiddle > 0)
+                {
+                    end = middle - 1;
+                }
+                else
+                {
+                    start = middle + 1;
+                }
+            }
+
+            return ~start;
+        }
+    }
+}
---
+++ Algorithms.Tests/BinarySearchTests.cs
@@ -0,0 +1,25 @@
+﻿using Algorithms.Solutions;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    [Trait("Category", "Unit")]
+    public class BinarySearchTests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 0, -1)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 1, 0)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 2, 1)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 3, 2)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 4, 3)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 5, 4)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 6, -5)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 7, -5)]
+        [InlineData(new[] { 1, 2, 3, 5, 6 }, 4, -4)]//~expected is where key should be inserted
+        public void Find_Sample_ExpectedIndex(int[] source, int key, int expected)
+        {
+            BinarySearch<int>.Find(source, key).Should().Be(expected);
+        }
+    }
+}
