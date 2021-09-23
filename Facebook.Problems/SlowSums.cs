ff1730916e0387ff2d241cfe217aa2170b65af31
Illia Levandovskyi <lis2889@gmail.com>
2021-09-23 16:12:49 +0300

    slow sums problem is added

---
+++ Facebook.Problems/SlowSums.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Facebook.Problems
+{
+    public class SlowSums
+    {
+        /// <summary>
+        /// the main issue here is to prove the solution is optimal, as it's a sort of greedy algorithm
+        /// </summary>
+        public static int GetTotalTime(int[] arr)
+        {
+            if (arr.Length < 2) return 0;
+
+            //O(N*logN) time; O(N) space
+            IReadOnlyList<int> descending = arr.OrderByDescending(n => n).ToArray();
+            long totalPenalty = 0, currentSum = descending[0];
+
+            for (int i = 1; i < descending.Count; i++)//O(N) time
+            {
+                currentSum += descending[i];
+                totalPenalty += currentSum;
+            }
+
+            return (int)(totalPenalty % int.MaxValue);//what about exception in case of overflow?
+        }
+    }
+}
---
+++ Facebook.Tests/SlowSumsTests.cs
@@ -0,0 +1,19 @@
+﻿using Facebook.Problems;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using Xunit;
+
+namespace Facebook.Tests
+{
+    [Trait("Category", "Unit")]
+    public class SlowSumsTests
+    {
+        [Theory]
+        [InlineData(new[]{1,2,3,4,5}, 50)]
+        [InlineData(new[]{4,2,3,1}, 26)]
+        public void GetTotalTime_Sample_MatchExpectations(int[] numbers, int expected)
+        {
+            SlowSums.GetTotalTime(numbers).Should().Be(expected);
+        }
+    }
+}
