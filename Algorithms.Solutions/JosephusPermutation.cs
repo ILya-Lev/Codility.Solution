11f820fa12eb55930b765482eeb902a11b84b7e0
Illia Levandovskyi <lis2889@gmail.com>
2021-09-30 14:38:09 +0300

    josephson permutation straightforward solution; look for sophisticated one!

---
+++ Algorithms.Solutions/JosephusPermutation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Solutions
+{
+    public class JosephusPermutation
+    {
+        public static int[] Construct(int n, int m)
+        {
+            if (m > n) throw new ArgumentException($"m must be less than n; but {m} > {n}");
+
+            int[] permutation = new int[n];
+            var current = 0;
+            var generation = 0;
+            while (current < n)
+            {
+                var number = n * generation + m * (current + 1) - current * (generation > 0 ? 1 : 0);
+                permutation[current++] = number % n;
+                generation = number / n;
+            }
+
+            return permutation;
+        }
+
+        public static int[] ConstructQueue(int n, int m)
+        {
+            if (m > n) throw new ArgumentException($"m must be less than n; but {m} > {n}");
+
+            int[] permutation = new int[n];
+            var queue = new Queue<int>(Enumerable.Range(1, n));
+
+            int counter = 0;
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                counter++;
+                
+                if (counter % m == 0)
+                    permutation[counter / m - 1] = current;//more simple is to use List.Add()
+                else
+                    queue.Enqueue(current);
+            }
+
+            return permutation;
+        }
+    }
+}
---
+++ Algorithms.Tests/JosephusPermutationTests.cs
@@ -0,0 +1,21 @@
+﻿using Algorithms.Solutions;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class JosephusPermutationTests
+    {
+        [Fact]
+        public void Construct_7And3_MatchExpectations()
+        {
+            JosephusPermutation.Construct(7, 3).Should().Equal(new[] { 3, 6, 2, 7, 5, 1, 4 });
+        }
+
+        [Fact]
+        public void ConstructQueue_7And3_MatchExpectations()
+        {
+            JosephusPermutation.ConstructQueue(7, 3).Should().Equal(new[] { 3, 6, 2, 7, 5, 1, 4 });
+        }
+    }
+}
