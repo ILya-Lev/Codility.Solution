65e83b5e607b880cd464e0321e317a5042240ab3
Illia Levandovskyi <lis2889@gmail.com>
2021-10-01 14:34:22 +0300

    josephon permutation without a queue

--- Algorithms.Solutions/JosephusPermutation.cs
+++ Algorithms.Solutions/JosephusPermutation.cs
@@ -7,23 +7,56 @@ namespace Algorithms.Solutions
 {
     public class JosephusPermutation
     {
+        /// <summary>
+        /// the most important part: i + n - eliminatedAmount
+        /// numbers between two eliminations should change its index to be (i + n - eliminatedAmount)
+        /// in case (7,3), when 3 is eliminated, 1 becomes 8, 2 becomes 9
+        /// </summary>
+        /// <exception cref="ArgumentException">when n less than m</exception>
         public static int[] Construct(int n, int m)
         {
             if (m > n) throw new ArgumentException($"m must be less than n; but {m} > {n}");
 
+            var numberByIndex = Enumerable.Range(1, n).ToDictionary(n => n, n => n);
+
             int[] permutation = new int[n];
-            var current = 0;
-            var generation = 0;
-            while (current < n)
+            var permutationIndex = 0;
+
+            var eliminatedAmount = 0;
+            var previousEliminated = 0;
+            var current = 1;
+            
+            while (permutationIndex < n)                                    //overall O(n*m) instead of n*n in 
             {
-                var number = n * generation + m * (current + 1) - current * (generation > 0 ? 1 : 0);
-                permutation[current++] = number % n;
-                generation = number / n;
+                if (current % m == 0)
+                {
+
+                    for (int i = previousEliminated + 1; i < current; i++)  //O(m) as current - previous = m
+                    {
+                        var value = numberByIndex[i];
+                        numberByIndex.Remove(i);                            //O(1) according to https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.dictionary-2.remove?view=net-5.0#System_Collections_Generic_Dictionary_2_Remove__0_
+                        numberByIndex.Add(i + n - eliminatedAmount, value); //O(1) asymptotically while capacity < n (it's true in this case)
+                    }
+
+                    permutation[permutationIndex++] = numberByIndex[current];
+
+                    eliminatedAmount++;
+                    previousEliminated = current;
+                }
+
+                current++;
             }
 
             return permutation;
         }
 
+        /// <summary>
+        /// O(n*m)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static int[] ConstructQueue(int n, int m)
         {
             if (m > n) throw new ArgumentException($"m must be less than n; but {m} > {n}");
@@ -32,11 +65,11 @@ namespace Algorithms.Solutions
             var queue = new Queue<int>(Enumerable.Range(1, n));
 
             int counter = 0;
-            while (queue.Count > 0)
+            while (queue.Count > 0)     //O(n*m)
             {
                 var current = queue.Dequeue();
                 counter++;
-                
+
                 if (counter % m == 0)
                     permutation[counter / m - 1] = current;//more simple is to use List.Add()
                 else
