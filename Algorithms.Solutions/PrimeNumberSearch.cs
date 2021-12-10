34f4d7369844758d873ff92217b2249ed82aca77
Illia Levandovskyi <lis2889@gmail.com>
2021-12-10 19:54:15 +0200

    prime number generation is added

---
+++ Algorithms.Solutions/PrimeNumberSearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Solutions
+{
+    public class PrimeNumberSearch
+    {
+        private static readonly List<long> _primes = new List<long>();
+
+        public IReadOnlyCollection<long> GetAllPrimesUpTo(long threshold) => GetAllPrimes()
+            .TakeWhile(p => p <= threshold)
+            .ToArray();
+
+        private IEnumerable<long> GetAllPrimes()
+        {
+            foreach (var prime in _primes)
+            {
+                yield return prime;
+            }
+
+            if (!_primes.Any())
+            {
+                _primes.Add(2);
+                _primes.Add(3);
+                yield return 2;
+                yield return 3;
+            }
+
+            for (var current = _primes.Last(); current > 0; current += 2)
+            {
+                if (IsPrime(current))
+                {
+                    _primes.Add(current);
+                    yield return current;
+                }
+            }
+        }
+
+        /// <summary> even numbers are not handled as it's not expected they will ever be passed inhere </summary>
+        private bool IsPrime(long current)
+        {
+            var limit = (long)Math.Sqrt(current) + 1;
+            foreach (var prime in _primes.TakeWhile(p => p <= limit))
+            {
+                if (current % prime == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Algorithms.Tests/PrimeNumberSearchTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Solutions;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Algorithms.Tests
+{
+    public class PrimeNumberSearchTests
+    {
+        private readonly ITestOutputHelper _output;
+        public PrimeNumberSearchTests(ITestOutputHelper output) => _output = output;
+
+        [Fact]
+        public void GetAllPrimesUpTo_Bln_Profile()
+        {
+            var searcher = new PrimeNumberSearch();
+            var primes = searcher.GetAllPrimesUpTo(100_000_000L);
+
+            primes.Should().NotBeEmpty();
+            _output.WriteLine($"{primes.Count} item is {primes.Last()}");
+        }
+    }
+}
