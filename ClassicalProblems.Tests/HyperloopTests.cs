14e0bdf8a34c485b9b0f46eff66aa86a71292b5c
Illia Levandovskyi <lis2889@gmail.com>
2022-01-04 23:31:22 +0200

    graph and mst

---
+++ ClassicalProblems.Tests/HyperloopTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ClassicalProblems.Tests;
+
+public class HyperloopTests
+{
+    private readonly ITestOutputHelper _output;
+    private readonly UnweightedGraph<string> _cityGraph;
+    private readonly WeightedGraph<string> _weightedCityGraph;
+
+    public HyperloopTests(ITestOutputHelper output)
+    {
+        _output = output;
+
+        var cities = new[]
+        {
+            "Boston", "NewYork", "Philadelphia", "Washington", "Miami", "Atlanta", "Detroit", "Chicago",
+            "Dallas", "Huston", "Phoenix", "Riverside", "LosAngeles", "SanFrancisco", "Seattle"
+        };
+        
+        _cityGraph = new UnweightedGraph<string>(cities);
+
+        _cityGraph.AddEdge("Boston", "NewYork");
+        _cityGraph.AddEdge("Boston", "Detroit");
+        _cityGraph.AddEdge("NewYork", "Detroit");
+        _cityGraph.AddEdge("NewYork", "Philadelphia");
+        _cityGraph.AddEdge("Washington", "Philadelphia");
+        _cityGraph.AddEdge("Washington", "Detroit");
+        _cityGraph.AddEdge("Washington", "Miami");
+        _cityGraph.AddEdge("Washington", "Atlanta");
+        _cityGraph.AddEdge("Miami", "Atlanta");
+        _cityGraph.AddEdge("Miami", "Huston");
+        _cityGraph.AddEdge("Atlanta", "Chicago");
+        _cityGraph.AddEdge("Atlanta", "Huston");
+        _cityGraph.AddEdge("Atlanta", "Dallas");
+        _cityGraph.AddEdge("Huston", "Dallas");
+        _cityGraph.AddEdge("Huston", "Phoenix");
+        _cityGraph.AddEdge("Dallas", "Phoenix");
+        _cityGraph.AddEdge("Dallas", "Chicago");
+        _cityGraph.AddEdge("Detroit", "Chicago");
+        _cityGraph.AddEdge("Riverside", "Chicago");
+        _cityGraph.AddEdge("Riverside", "Phoenix");
+        _cityGraph.AddEdge("Riverside", "LosAngeles");
+        _cityGraph.AddEdge("Riverside", "SanFrancisco");
+        _cityGraph.AddEdge("Phoenix", "LosAngeles");
+        _cityGraph.AddEdge("SanFrancisco", "LosAngeles");
+        _cityGraph.AddEdge("SanFrancisco", "Seattle");
+        _cityGraph.AddEdge("Chicago", "Seattle");
+        
+        _weightedCityGraph = new WeightedGraph<string>(cities);
+        _weightedCityGraph.AddEdge("Boston", "NewYork", 190);
+        _weightedCityGraph.AddEdge("Boston", "Detroit", 613);
+        _weightedCityGraph.AddEdge("NewYork", "Detroit", 482);
+        _weightedCityGraph.AddEdge("NewYork", "Philadelphia", 81);
+        _weightedCityGraph.AddEdge("Washington", "Philadelphia", 123);
+        _weightedCityGraph.AddEdge("Washington", "Detroit", 396);
+        _weightedCityGraph.AddEdge("Washington", "Miami", 923);
+        _weightedCityGraph.AddEdge("Washington", "Atlanta", 543);
+        _weightedCityGraph.AddEdge("Miami", "Atlanta", 604);
+        _weightedCityGraph.AddEdge("Miami", "Huston", 968);
+        _weightedCityGraph.AddEdge("Atlanta", "Chicago", 588);
+        _weightedCityGraph.AddEdge("Atlanta", "Huston", 702);
+        _weightedCityGraph.AddEdge("Atlanta", "Dallas", 721);
+        _weightedCityGraph.AddEdge("Huston", "Dallas", 225);
+        _weightedCityGraph.AddEdge("Huston", "Phoenix", 1015);
+        _weightedCityGraph.AddEdge("Dallas", "Phoenix", 887);
+        _weightedCityGraph.AddEdge("Dallas", "Chicago", 805);
+        _weightedCityGraph.AddEdge("Detroit", "Chicago", 238);
+        _weightedCityGraph.AddEdge("Riverside", "Chicago", 1704);
+        _weightedCityGraph.AddEdge("Riverside", "Phoenix", 307);
+        _weightedCityGraph.AddEdge("Riverside", "LosAngeles", 50);
+        _weightedCityGraph.AddEdge("Riverside", "SanFrancisco", 386);
+        _weightedCityGraph.AddEdge("Phoenix", "LosAngeles", 357);
+        _weightedCityGraph.AddEdge("SanFrancisco", "LosAngeles", 348);
+        _weightedCityGraph.AddEdge("SanFrancisco", "Seattle", 678);
+        _weightedCityGraph.AddEdge("Chicago", "Seattle", 1737);
+    }
+
+    [Fact]
+    public void ToString_15Cities_Observe() => _output.WriteLine(_cityGraph.ToString());
+
+    [Fact]
+    public void FindShortestPath_BostonToMiami_ThroughDetroit()
+    {
+        var path = _cityGraph.FindShortestPath("Boston", "Miami");
+        path.Should().HaveCount(4);
+        path.Should().Contain("Detroit");
+        _output.WriteLine(string.Join(" -> ", path));
+    }
+    
+    [Fact]
+    public void ToString_15WeightedCities_Observe() => _output.WriteLine(_weightedCityGraph.ToString());
+
+    [Fact]
+    public void Mst_Build_Print()
+    {
+        var tree = _weightedCityGraph.BuildMinimumSpanningTree();
+        tree.Should().HaveCount(14);
+        var report = _weightedCityGraph.ConvertMstToString(tree);
+        _output.WriteLine(report);
+    }
+}
---
+++ ClassicalProblems/Graph.cs
@@ -0,0 +1,180 @@
+﻿using System.Text;
+
+namespace ClassicalProblems;
+
+public class Edge
+{
+    public int From { get; }
+    public int To { get; }
+    public Edge(int from, int to) => (From, To) = (from, to);
+    public virtual Edge CreateReversed() => new (To, From);
+    public override string ToString() => $"{From:D3} -> {To:D3}";
+}
+
+public class WeightedEdge : Edge, IComparable<WeightedEdge>
+{
+    public double Weight { get;}
+    public WeightedEdge(int from, int to, double weight) : base(from, to) => Weight = weight;
+    public int CompareTo(WeightedEdge other) => Weight.CompareTo(other.Weight);
+    public override Edge CreateReversed() => new WeightedEdge(To, From, Weight);
+    public override string ToString() => $"{From:D3} {Weight:N}> {To:D3}";
+}
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="V">type of Vertex</typeparam>
+/// <typeparam name="E">type of Edge, should inherit class Edge</typeparam>
+public abstract class Graph<V, E>
+    where V : notnull
+    where E : Edge
+{
+    private readonly List<V> _vertices;
+    private readonly Dictionary<V, int> _indexByVertex;
+    protected readonly List<List<E>> _edges;
+
+    public int VertexCount => _vertices.Count;
+    public int EdgeCount => _edges.Sum(e => e.Count);
+    public V this[int index] => _vertices[index];
+
+    protected Graph(IEnumerable<V> vertices)
+    {
+        _vertices = new List<V>(vertices);
+        _indexByVertex = _vertices.Select((v, i) => (v, i)).ToDictionary(p => p.v, p => p.i);
+        _edges = Enumerable.Repeat(1, _vertices.Count).Select(n => new List<E>()).ToList();
+    }
+
+    /// <summary> add new vertex and return its index in the list of vertices </summary>
+    public int AddVertex(V vertex)
+    {
+        _vertices.Add(vertex);
+        _indexByVertex.Add(vertex, _vertices.Count - 1);
+        _edges.Add(new List<E>());
+        return _vertices.Count - 1;
+    }
+
+    public int GetIndexOf(V vertex) => _indexByVertex[vertex];//O(1)
+
+    public IReadOnlyList<V> GetNeighborsOf(int index) => _edges[index].Select(e => this[e.To]).ToArray();
+    public IReadOnlyList<V> GetNeighborsOf(V vertex) => GetNeighborsOf(GetIndexOf(vertex));
+
+    public IReadOnlyList<E> GetEdgesOf(int index) => _edges[index];
+    public IReadOnlyList<E> GetEdgesOf(V vertex) => _edges[GetIndexOf(vertex)];
+
+    public abstract void AddEdge(E edge);
+    
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < VertexCount; i++)
+        {
+            sb.Append(this[i]);
+            sb.Append(" -> ");
+            sb.Append(string.Join(", ", GetNeighborsOf(i).Select(n => n.ToString())));
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+}
+
+public class UnweightedGraph<V> : Graph<V, Edge>
+{
+    public UnweightedGraph(IEnumerable<V> vertices) : base(vertices) { }
+
+    //as the graph is not oriented
+    public override void AddEdge(Edge edge)
+    {
+        _edges[edge.From].Add(edge);
+        _edges[edge.To].Add(edge.CreateReversed());
+    }
+
+    public void AddEdge(int from, int to) => AddEdge(new Edge(@from, to));
+    public void AddEdge(V from, V to) => AddEdge(new Edge(GetIndexOf(@from), GetIndexOf(to)));
+
+    public IReadOnlyCollection<V> FindShortestPath(V from, V to)
+    {
+        Func<V, bool> isFound = v => v.Equals(to);
+
+        var (lastNode, _) = SearchNode<V>.BreadthFirstSearch(from, isFound, GetNeighborsOf);
+
+        var path = SearchNode<V>.AsPath(lastNode);
+
+        return path;
+    }
+}
+
+public class WeightedGraph<V> : Graph<V, WeightedEdge>
+{
+    public WeightedGraph(IEnumerable<V> vertices) : base(vertices) { }
+
+    public override void AddEdge(WeightedEdge edge)
+    {
+        _edges[edge.From].Add(edge);
+        _edges[edge.To].Add(edge.CreateReversed() as WeightedEdge);
+    }
+
+    public void AddEdge(int from, int to, double weight) => AddEdge(new WeightedEdge(from, to, weight));
+
+    public void AddEdge(V from, V to, double weight) => AddEdge(GetIndexOf(from), GetIndexOf(to), weight);
+    
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < VertexCount; i++)
+        {
+            sb.Append(this[i]);
+            sb.Append(" -> ");
+            sb.Append(string.Join(", ", GetEdgesOf(i).Select(e => $"({this[e.To]}, {e.Weight})")));
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+    
+    /// <summary>
+    /// algorithm by Prim or Jarnik
+    /// does not work with directed edges and unbounded graphs
+    /// </summary>
+    /// <returns></returns>
+    public List<WeightedEdge> BuildMinimumSpanningTree()
+    {
+        var tree = new List<WeightedEdge>();
+
+        var seen = new HashSet<int>();
+        var superEdge = new PriorityQueue<WeightedEdge, double>();
+        
+        var seedIndex = new Random(DateTime.UtcNow.Millisecond).Next(0, VertexCount);
+        
+        seen.Add(seedIndex);
+        superEdge.EnqueueRange(OutboundEdges(seedIndex));
+
+        while (seen.Count < VertexCount)
+        {
+            var shortestEdge = superEdge.Dequeue();
+            if (seen.Contains(shortestEdge.To)) continue;
+            tree.Add(shortestEdge);
+            seen.Add(shortestEdge.To);
+            superEdge.EnqueueRange(OutboundEdges(shortestEdge.To));
+        }
+
+        return tree;
+
+        IEnumerable<(WeightedEdge e, double Weight)> OutboundEdges(int vertexIndex) => 
+            GetEdgesOf(vertexIndex)
+            .Where(e => !seen.Contains(e.To))
+            .Select(e => (e, e.Weight));
+    }
+
+    public string ConvertMstToString(IReadOnlyCollection<WeightedEdge> tree)
+    {
+        var sb = new StringBuilder();
+        foreach (var edge in tree)
+        {
+            sb.AppendLine($"{this[edge.From]} {edge.Weight}> {this[edge.To]}");
+        }
+
+        sb.AppendLine($"Total Weight: {tree.Sum(e => e.Weight)}");
+        return sb.ToString();
+    }
+}
