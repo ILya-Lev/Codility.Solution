99fd8f48d6d41ffc98c70965d0f0b63d930290f6
Illia Levandovskyi <lis2889@gmail.com>
2022-01-15 18:45:34 +0200

    k-means algo is completed

---
+++ ClassicalProblems.Tests/KMeansTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ClassicalProblems.Tests;
+
+public class KMeansTests
+{
+    private readonly ITestOutputHelper _output;
+    public KMeansTests(ITestOutputHelper output) => _output = output;
+
+    [Fact]
+    public void Run_3DataPoints_2Clusters()
+    {
+        var sut = new KMeans<DataPoint>(2, new[]
+        {
+            new DataPoint(new[] { 2.0, 1.0, 1.0 }),
+            new DataPoint(new[] { 2.0, 2.0, 5.0 }),
+            new DataPoint(new[] { 3.0, 1.5, 2.5 }),
+        });
+
+        var clusters = sut.Run(100);
+
+        clusters.Should().HaveCount(2);
+        foreach (var cluster in clusters)
+        {
+            _output.WriteLine($"Cluster in {cluster.Centroid} contains {cluster.Points.Count} points: ");
+            foreach (var point in cluster.Points)
+            {
+                _output.WriteLine($"{point}");
+            }
+        }
+    }
+}
--- ClassicalProblems/Statistics.cs
+++ ClassicalProblems/Statistics.cs
@@ -52,9 +52,23 @@ public class Statistics
 
 public class DataPoint
 {
+    private readonly List<double> _dimensions;
+    public int DimensionsCount => Dimensions.Count;
+    public IReadOnlyList<double> Dimensions => _dimensions;
+
     public IReadOnlyList<double> Originals { get; }
 
-    public DataPoint(IEnumerable<double> initials) => Originals = initials.ToArray();
+    public DataPoint(IEnumerable<double> initials)
+    {
+        Originals = initials.ToArray();
+        _dimensions = Originals.ToList();//deep copy
+    }
+
+    public void RewriteDimensions(IEnumerable<double> dimensions)
+    {
+        _dimensions.Clear();
+        _dimensions.AddRange(dimensions);
+    }
 
     public double GetDistance(DataPoint other)
     {
@@ -66,4 +80,143 @@ public class DataPoint
     }
 
     public override string ToString() => string.Join(", ", Originals.Select(p => $"{p}"));
+}
+
+public class KMeans<Point> where Point : DataPoint
+{
+    private readonly IReadOnlyList<Point> _points;
+    private readonly List<Cluster> _clusters;
+
+    public IReadOnlyList<DataPoint> Centroids => _clusters.Select(c => c.Centroid).ToArray();
+
+    public KMeans(int k, IEnumerable<Point> points)
+    {
+        if (k < 1) throw new ArgumentException($"Cluster's count has to be above zero; provided {k}");
+
+        _points = points.ToArray();
+
+        ZScoreNormalize();
+
+        _clusters = Enumerable.Range(1, k).Select(n => new Cluster(CreateRandomPoint())).ToList();
+    }
+
+    public IReadOnlyList<Cluster> Run(int maxIterations)
+    {
+        for (int iteration = 0; iteration < maxIterations; iteration++)
+        {
+            foreach (var cluster in _clusters)
+                cluster.Points.Clear();
+
+            AssignClusters();
+            var previous = Centroids;
+            GenerateCentroids();
+            var current = Centroids;
+            if (AreSetsEqual(previous, current))
+                break;
+        }
+
+        return _clusters;
+    }
+
+    private void ZScoreNormalize()
+    {
+        var zScores = _points.Select(_ => new List<double>()).ToArray();
+
+        //instead of using a value in a DataPoint, use its z-score, but not in values from the point
+        //but in perpendicular values set (so called slice)
+        for (var dimension = 0; dimension < _points[0].DimensionsCount; dimension++)
+        {
+            var slice = GetDimensionSlice(_points, dimension);
+            var stat = new Statistics(slice);
+            var zScoredSlice = stat.GetZScoredSequence();
+
+            for (int pointIndex = 0; pointIndex < zScoredSlice.Count; pointIndex++)
+                zScores[pointIndex].Add(zScoredSlice[pointIndex]);
+        }
+
+        //assign z-scores from collector (zScores variable) back into each point's dimension property
+        for (int pointIndex = 0; pointIndex < zScores.Length; pointIndex++)
+            _points[pointIndex].RewriteDimensions(zScores[pointIndex]);
+    }
+
+    private DataPoint CreateRandomPoint()
+    {
+        var random = new Random(DateTime.UtcNow.Millisecond);
+        var randomPointValues = new List<double>();
+        for (int dimension = 0; dimension < _points[0].DimensionsCount; dimension++)
+        {
+            var slice = GetDimensionSlice(_points, dimension);
+            var stat = new Statistics(slice);
+            
+            var min = stat.GetMin();
+            var max = stat.GetMax();
+            var rand = random.NextDouble();
+
+            var value = max * rand + (1 - rand) * min;
+            randomPointValues.Add(value);
+        }
+
+        return new DataPoint(randomPointValues);
+    }
+
+    private IReadOnlyList<double> GetDimensionSlice(IEnumerable<Point> points, int dimension) => 
+        points.Select(p => p.Dimensions[dimension]).ToArray();
+
+    private void AssignClusters()
+    {
+        foreach (var point in _points)
+        {
+            var closestCluster = _clusters[0];
+            var minDistance = double.MaxValue;
+            foreach (var cluster in _clusters)
+            {
+                var currentDistance = point.GetDistance(cluster.Centroid);
+                if (currentDistance < minDistance)
+                {
+                    minDistance = currentDistance;
+                    closestCluster = cluster;
+                }
+            }
+            closestCluster.Points.Add(point);
+        }
+    }
+
+    private void GenerateCentroids()
+    {
+        foreach (var cluster in _clusters)
+        {
+            if (!cluster.Points.Any()) continue;
+
+            var centroidPoints = new List<double>();
+            
+            //calculate average for the cluster's slice
+            for (int dimension = 0; dimension < cluster.Points[0].DimensionsCount; dimension++)
+            {
+                var sliceMean = GetDimensionSlice(cluster.Points, dimension).Average();
+                centroidPoints.Add(sliceMean);
+            }
+
+            cluster.Centroid = new DataPoint(centroidPoints);
+        }
+    }
+
+    private bool AreSetsEqual(IReadOnlyCollection<DataPoint> lhs, IReadOnlyCollection<DataPoint> rhs)
+    {
+        if (lhs.Count != rhs.Count) return false;
+        return lhs
+            .Zip(rhs, (l, r) => l.Dimensions.Zip(r.Dimensions, (v1, v2) => v1.Equals(v2)).All(areSame => areSame))
+            .All(areSame => areSame);
+    }
+
+    public class Cluster
+    {
+        public List<Point> Points { get; } = new List<Point>();
+        public DataPoint Centroid { get; set; }
+
+        public Cluster(DataPoint initialCentroid)
+        {
+            Centroid = initialCentroid
+                       ?? throw new ArgumentNullException(nameof(initialCentroid), "Please provide non null centroid");
+        }
+    }
 }
