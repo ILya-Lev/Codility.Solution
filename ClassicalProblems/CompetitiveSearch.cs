615fee7ea933f8a54448bc827cbc856331cec18c
Illia Levandovskyi <lis2889@gmail.com>
2022-01-29 18:17:13 +0200

    connect four game implementation

--- ClassicalProblems/CompetitiveSearch.cs
+++ ClassicalProblems/CompetitiveSearch.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace ClassicalProblems;
 
@@ -51,12 +52,12 @@ public static class CompetitiveSearch
                 return board.Evaluate(originalPlayer);
 
             var moves = board.GetLegalMoves();
-            
+
             var finalScore = isMaximizing ? double.MinValue : double.MaxValue;
             foreach (var move in moves)
             {
                 var evaluation = MiniMaxDepthSearch(board.Move(move), !isMaximizing, originalPlayer, remainingDepth - 1);
-                finalScore = isMaximizing 
+                finalScore = isMaximizing
                     ? Math.Max(finalScore, evaluation)
                     : Math.Min(finalScore, evaluation);
             }
@@ -179,6 +180,227 @@ public static class CompetitiveSearch
                 return true;
 
             return false;
+        }
+    }
+
+    public class ConnectFourPiece : IPiece
+    {
+        private readonly string _name;
+        public static IPiece Black { get; } = new ConnectFourPiece("B");
+        public static IPiece Red { get; } = new ConnectFourPiece("R");
+        public static IPiece Empty { get; } = new ConnectFourPiece(" ");
+
+        private ConnectFourPiece(string name) => _name = name;
+
+        public IPiece GetOpposite() =>
+            this == Black
+                ? Red
+                : this == Red
+                    ? Black
+                    : Empty;
+
+        public override string ToString() => _name;
+    }
+
+    //7 columns and 6 rows
+    public class ConnectFourBoard : IBoard<int>
+    {
+        public const int CellAmount = 42;
+        private const int Width = 7;
+        private const int WinningSet = 4;
+
+        private static IReadOnlyList<Segment> Segments { get; }
+
+        private readonly ConnectFourPiece[] _positions;
+        private readonly ConnectFourPiece _turn;
+
+        static ConnectFourBoard()
+        {
+            Segments = Enumerable.Range(0, CellAmount - 1)
+                .SelectMany(location => new[]
+                {
+                    Segment.CreateInRow(location),
+                    Segment.CreateInColumn(location),
+                    Segment.CreateInMainDiagonal(location),
+                    Segment.CreateInMinorDiagonal(location),
+                })
+                .Where(s => s is not null)
+                .Distinct()
+                .ToArray()!;
+        }
+
+
+        //the very first turn game state
+        public ConnectFourBoard()
+        {
+            _positions = Enumerable.Range(1, CellAmount).Select(_ => (ConnectFourPiece)ConnectFourPiece.Empty).ToArray();//the field is empty
+            _turn = ConnectFourPiece.Black as ConnectFourPiece;//first player puts Black pieces
+        }
+
+        public ConnectFourBoard(ConnectFourPiece[] positions, ConnectFourPiece turn)
+        {
+            _positions = positions;
+            _turn = turn ?? throw new ArgumentNullException(nameof(turn));
+        }
+
+        public IPiece GetTurn() => _turn;
+
+        public IBoard<int> Move(int column)
+        {
+            var nextPositions = _positions.ToArray();//deep copy
+            var firstEmptyRow = FindEmptyRow(column);
+            var location = GetLocation(firstEmptyRow, column);
+            nextPositions[location] = _turn;
+            return new ConnectFourBoard(nextPositions, _turn.GetOpposite() as ConnectFourPiece);
+        }
+
+        public IReadOnlyList<int> GetLegalMoves()
+        {
+            if (IsWin())
+                return Array.Empty<int>();
+
+            var topRowPieceWithIndex = _positions.TakeLast(Width).Select((p, i) => (p, i));
+
+            var topRowEmptyPieceColumns = topRowPieceWithIndex
+                .Where(t => t.p == ConnectFourPiece.Empty)
+                .Select(t => t.i)
+                .ToArray();
+
+            return topRowEmptyPieceColumns;
+        }
+
+        public bool IsWin()
+        {
+            return Segments.Any(s => AreAllTheSameFill(s, ConnectFourPiece.Black as ConnectFourPiece) 
+                                  || AreAllTheSameFill(s, ConnectFourPiece.Red as ConnectFourPiece));
+        }
+
+        public double Evaluate(IPiece player) => Segments.Sum(EvaluateSegment);
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int row = 0; row < CellAmount/Width; row++)
+            {
+                for (int col = 0; col < Width; col++)
+                {
+                    var location = row * Width + col;
+                    sb.Append(_positions[location].ToString());
+                    sb.Append("|");
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
+        private double EvaluateSegment(Segment segment)
+        {
+            var blackCount = GetMatchingCount(segment, ConnectFourPiece.Black as ConnectFourPiece);
+            var redCount = GetMatchingCount(segment, ConnectFourPiece.Red as ConnectFourPiece);
+
+            if (blackCount > 0 && redCount > 0) return 0.0;
+
+            var score = Math.Max(blackCount, redCount) switch
+            {
+                2 => 1.0,
+                3 => 100.0,
+                4 => 1_000_000.0,
+                _ => 0.0
+            };
+
+            var negate = (blackCount > redCount && _turn == ConnectFourPiece.Red)
+                      || (blackCount < redCount && _turn == ConnectFourPiece.Black);
+            
+            return negate ? -score : score;
+        }
+
+        private bool AreAllTheSameFill(Segment segment, ConnectFourPiece piece)
+        {
+            return GetMatchingCount(segment, piece) == segment.Locations.Count;
+        }
+
+        private int GetMatchingCount(Segment segment, ConnectFourPiece piece)
+        {
+            return segment.Locations.Count(l => _positions[l] == piece);
+        }
+
+        private int FindEmptyRow(int column, [CallerMemberName] string caller = "")
+        {
+            for (int row = 0; row < CellAmount / Width; row++)
+            {
+                var location = GetLocation(row, column);
+                if (_positions[location] == ConnectFourPiece.Empty)
+                    return row;
+            }
+
+            throw new ArgumentException($"Column {column} is already full - {caller} should have not been called for it!");
+        }
+
+        private static int GetLocation(int row, int column) => row * Width + column;
+
+        private class Segment
+        {
+            private const int CoordinateShift = 3;
+
+            public IReadOnlyList<int> Locations { get; }
+
+            private Segment(int[] locations) => Locations = locations;
+
+            public static Segment? CreateInRow(int location)
+            {
+                var endColumn = location % Width + CoordinateShift;
+                if (endColumn >= Width) return null;
+
+                var locations = Enumerable.Range(location, CoordinateShift + 1).ToArray();
+
+                return new Segment(locations);
+            }
+
+            public static Segment? CreateInColumn(int location)
+            {
+                var endRow = location / Width + CoordinateShift;
+                if (endRow >= CellAmount / Width) return null;
+
+                var col = location % Width;
+                var row = location / Width;
+                var locations = Enumerable.Range(0, CoordinateShift + 1).Select(shift => (row + shift) * Width + col).ToArray();
+
+                return new Segment(locations);
+            }
+
+            public static Segment? CreateInMainDiagonal(int location)
+            {
+                var endRow = location / Width + CoordinateShift;
+                if (endRow >= CellAmount / Width) return null;
+
+                var endColumn = location % Width + CoordinateShift;
+                if (endColumn >= Width) return null;
+
+                var row = location / Width;
+                var col = location % Width;
+                
+                var locations = Enumerable.Range(0, CoordinateShift + 1).Select(shift => (row + shift) * Width + (col+shift)).ToArray();
+
+                return new Segment(locations);
+            }
+
+            public static Segment? CreateInMinorDiagonal(int location)
+            {
+                var endRow = location / Width + CoordinateShift;
+                if (endRow >= CellAmount / Width) return null;
+
+                var endColumn = location % Width - CoordinateShift;
+                if (endColumn >= Width) return null;
+
+                var row = location / Width;
+                var col = location % Width;
+                
+                var locations = Enumerable.Range(0, CoordinateShift + 1).Select(shift => (row + shift) * Width + (col-shift)).ToArray();
+
+                return new Segment(locations);
+            }
         }
     }
 }
---
+++ ConnectFour/Program.cs
@@ -0,0 +1,85 @@
+﻿using static ClassicalProblems.CompetitiveSearch;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var positions = Enumerable.Range(1, ConnectFourBoard.CellAmount).Select(_ => (ConnectFourPiece)ConnectFourPiece.Empty).ToArray();
+        var board = new ConnectFourBoard(positions, (ConnectFourPiece)ConnectFourPiece.Black);
+
+        var (human, computer) = SelectSide();
+
+        var moveNumber = 0;
+        while (board.GetLegalMoves().Any())
+        {
+            Console.Clear();
+            Console.WriteLine($"step#{moveNumber}");
+            Console.WriteLine(board.ToString());
+
+            var location = board.GetTurn() == human
+                ? DoHumanMove(board, human)
+                : DoComputerMove(board);
+            moveNumber++;
+            board = board.Move(location) as ConnectFourBoard;
+        }
+
+        PrintResult(moveNumber, board, human);
+    }
+
+    private static (ConnectFourPiece human, ConnectFourPiece computer) SelectSide()
+    {
+        Console.WriteLine("Would you like to play with black pieces? [y/n]: ");
+        var response = Console.ReadKey();
+        var human = response.KeyChar == 'y' || response.KeyChar == 'Y' ? ConnectFourPiece.Black : ConnectFourPiece.Red;
+        var computer = human.GetOpposite() as ConnectFourPiece;
+        return ((ConnectFourPiece)human, computer!);
+    }
+
+    private static int DoHumanMove(ConnectFourBoard board, ConnectFourPiece human)
+    {
+        var location = -1;
+        var legalMoves = new HashSet<int>(board.GetLegalMoves());
+        var availableMoves = string.Join(",", board.GetLegalMoves().Select(m => $"{m}"));
+        while (!legalMoves.Contains(location))
+        {
+            Console.WriteLine($"please, enter where to put next {human} [{availableMoves}]");
+            location = int.TryParse(new string(Console.ReadKey().KeyChar, 1), out var loc) ? loc : -1;
+        }
+
+        return location;
+    }
+
+    private static int DoComputerMove(ConnectFourBoard board) => MiniMax.FindBestMove(board, 5);
+
+    private static void PrintResult(int moveNumber, ConnectFourBoard board, ConnectFourPiece human)
+    {
+        Console.Clear();
+        Console.WriteLine($"After {moveNumber} steps");
+        Console.WriteLine(board.ToString());
+        if (board.IsWin())
+        {
+            var winnerName = board.GetTurn() == human ? "computer" : "human";
+            Console.WriteLine($"{winnerName} wins!");
+        }
+        else
+        {
+            Console.WriteLine("It's a draw!");
+        }
+    }
+}
+
+/*
+ *
+ * max depth = 5
+After 33 steps
+B|B|B|R|R|R|B|
+R|B|R|B| |R|B|
+R|B|B|R| |B|R|
+R|R|B|B| |R|B|
+B|R|B| | |R| |
+B|R|R| | |B| |
+      ^           the winning move
+human wins!
+*/
