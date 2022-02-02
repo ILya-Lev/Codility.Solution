e8c1a9334018d62b2dfd6266eda921800b12f904
Illia Levandovskyi <lis2889@gmail.com>
2022-02-02 22:52:07 +0200

    travelling salesman problem - generate all permutations

---
+++ ClassicalProblems.Tests/TravelingSalesmanProblemTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ClassicalProblems.Tests;
+
+public class TravelingSalesmanProblemTests
+{
+    private readonly ITestOutputHelper _output;
+    public TravelingSalesmanProblemTests(ITestOutputHelper output) => _output = output;
+
+    [Theory]
+    [InlineData(3,6)]
+    [InlineData(4,24)]
+    [InlineData(5,120)]
+    public void GeneratePermutations_Items_Sequences(int size, int count)
+    {
+        var sequence = Enumerable.Range(1,size).ToArray();
+        var permutations = TravelingSalesmanProblem.GenerateAllPermutations(sequence);
+
+        using var scope = new AssertionScope();
+        new HashSet<int[]>(permutations).Should().HaveCount(count);
+        foreach (var permutation in permutations)
+        {
+            _output.WriteLine(string.Join(", ", permutation.Select(n => $"{n}")));
+        }
+    }
+}
---
+++ ClassicalProblems/TravelingSalesmanProblem.cs
@@ -0,0 +1,78 @@
+﻿namespace ClassicalProblems;
+
+/// <summary>
+/// in short TSP, zadacha komivoyajora
+/// </summary>
+public class TravelingSalesmanProblem
+{
+    private readonly IReadOnlyDictionary<string, IReadOnlyDictionary<string, int>> _distances =
+        new Dictionary<string, IReadOnlyDictionary<string, int>>(StringComparer.OrdinalIgnoreCase)
+    {
+        ["Ratland"] = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase)
+        {
+            ["Berlingtone"] = 67,
+            ["White-Riwer"] = 46,
+            ["Benningtone"] = 55,
+            ["Brattlboro"] = 75,
+        },
+        ["Berlingtone"] = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase)
+        {
+            ["Ratland"] = 67,
+            ["White-Riwer"] = 91,
+            ["Benningtone"] = 122,
+            ["Brattlboro"] = 153,
+        },
+        ["White-Riwer"] = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase)
+        {
+            ["Ratland"] = 46,
+            ["Berlingtone"] = 91,
+            ["Benningtone"] = 98,
+            ["Brattlboro"] = 65,
+        },
+        ["Benningtone"] = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase)
+        {
+            ["Ratland"] = 55,
+            ["Berlingtone"] = 122,
+            ["White-Riwer"] = 98,
+            ["Brattlboro"] = 40,
+        },
+        ["Brattlboro"] = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase)
+        {
+            ["Ratland"] = 75,
+            ["Berlingtone"] = 153,
+            ["White-Riwer"] = 65,
+            ["Benningtone"] = 40,
+        },
+    };
+
+    //public TravelingSalesmanProblem(IReadOnlyDictionary<string, IReadOnlyDictionary<string, int>> distances)
+    //{
+    //    _distances = distances;
+    //}
+
+    public static IReadOnlyList<T[]> GenerateAllPermutations<T>(T[] sequence)
+    {
+        var storage = new List<T[]>();
+        GenerateAllPermutations(sequence, storage, sequence.Length - 1);
+        return storage.ToArray();
+    }
+
+    private static void GenerateAllPermutations<T>(T[] sequence, List<T[]> permutations, int endIndex)
+    {
+        if (endIndex < 0)
+        {
+            permutations.Add(sequence);//another one is built
+            return;
+        }
+
+        var tmp = sequence.ToArray();//deep copy
+        for (int i = 0; i <= endIndex; i++)
+        {
+            (tmp[i], tmp[endIndex]) = (tmp[endIndex], tmp[i]);
+
+            GenerateAllPermutations<T>(tmp, permutations, endIndex-1);//all possible permutations if ith is in the end
+
+            (tmp[endIndex], tmp[i]) = (tmp[i], tmp[endIndex]);
+        }
+    }
+}
