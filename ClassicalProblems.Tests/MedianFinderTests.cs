8256507228143009dddad072c45b4eb5aef9b050
Illia Levandovskyi <lis2889@gmail.com>
2022-05-06 11:55:51 +0300

    median finder for an async stream impl

---
+++ ClassicalProblems.Tests/MedianFinderTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ClassicalProblems.Tests;
+
+public class MedianFinderTests
+{
+    private readonly ITestOutputHelper _output;
+    public MedianFinderTests(ITestOutputHelper output) => _output = output;
+
+    [Fact]
+    public async Task GetMedianPrices_Repeat_SameValue()
+    {
+        var numbers = Enumerable.Repeat(5m, 1000).ToArray();
+        var asyncSequence = GenerateSequence(numbers);
+        var medians = MedianFinder.GetMedianPrices(asyncSequence);
+
+        using var scope = new AssertionScope();
+        await foreach (var m in medians)
+            m.Should().Be(5m);
+    }
+
+    [Fact]
+    public async Task GetMedianPrices_ArithmeticalSeq_Observe()
+    {
+        var numbers = Enumerable.Range(1, 10).Select(n => (decimal)n).ToArray();
+        var asyncSequence = GenerateSequence(numbers);
+        var medians = MedianFinder.GetMedianPrices(asyncSequence);
+
+        var medianList = new List<decimal>();
+        await foreach (var m in medians)
+            medianList.Add(m);
+
+        medianList.Should().Equal(new decimal[] { 1, 1.5m, 2, 2.5m, 3, 3.5m, 4, 4.5m, 5, 5.5m });
+    }
+
+    private async IAsyncEnumerable<decimal> GenerateSequence(IEnumerable<decimal> numbers)
+    {
+        await Task.Yield();
+
+        foreach (var n in numbers)
+        {
+            yield return (decimal)n;
+        }
+    }
+}
---
+++ ClassicalProblems/MedianFinder.cs
@@ -0,0 +1,53 @@
+﻿namespace ClassicalProblems;
+
+public static class MedianFinder
+{
+    public static async IAsyncEnumerable<decimal> GetMedianPrices(IAsyncEnumerable<decimal> rawPrices)
+    {
+        PriorityQueue<decimal, decimal> headMax = new(new DescendingComparer<decimal>()),
+                                        tailMin = new(new AscendingComparer<decimal>());
+        await foreach (var price in rawPrices)
+        {
+            if (headMax.Count == 0 || headMax.Peek() >= price)
+            {
+                headMax.Enqueue(price, price);
+                MaintainCountEquilibrium(headMax, tailMin);
+                yield return CalculateMedian(headMax, tailMin);
+            }
+            else
+            {
+                tailMin.Enqueue(price, price);
+                MaintainCountEquilibrium(headMax, tailMin);
+                yield return CalculateMedian(tailMin, headMax);
+            }
+        }
+    }
+
+    private static decimal CalculateMedian(PriorityQueue<decimal, decimal> lead, PriorityQueue<decimal, decimal> follow)
+        => lead.Count == follow.Count
+         ? (lead.Peek() + follow.Peek()) / 2
+         : lead.Peek();
+
+    private static void MaintainCountEquilibrium(PriorityQueue<decimal, decimal> headMax, PriorityQueue<decimal, decimal> tailMin)
+    {
+        if (headMax.Count > tailMin.Count + 1)
+        {
+            var headTop = headMax.Dequeue();
+            tailMin.Enqueue(headTop, headTop);
+        }
+        else if (headMax.Count + 1 < tailMin.Count)
+        {
+            var tailBottom = tailMin.Dequeue();
+            headMax.Enqueue(tailBottom, tailBottom);
+        }
+    }
+}
+
+public class DescendingComparer<T> : IComparer<T> where T : notnull, IComparable<T>
+{
+    public int Compare(T lhs, T rhs) => rhs.CompareTo(lhs);
+}
+public class AscendingComparer<T> : IComparer<T> where T : notnull, IComparable<T>
+{
+    public int Compare(T lhs, T rhs) => lhs.CompareTo(rhs);
+}
