99a71b610eacf9a500cfdb9170d7928588298e7e
Illia Levandovskyi <lis2889@gmail.com>
2022-06-03 10:43:16 +0300

    p0024 solved accepted

---
+++ LeetCode.Tasks/P0024SwapPairs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode.Tasks;
+
+public class P0024SwapPairs
+{
+    public ListNode SwapPairs(ListNode head)
+    {
+        if (head?.next is null)
+            return head;
+
+        var h = head.next;
+        var e = head;
+
+        var remainder = h.next;
+
+        h.next = e;
+        e.next = SwapPairs(remainder);
+
+        return h;
+    }
+}
