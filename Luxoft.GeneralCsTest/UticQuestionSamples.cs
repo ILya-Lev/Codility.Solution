b2b8ca93ed290c987383b587fd0e4ce3367d5a0f
Illia Levandovskyi <lis2889@gmail.com>
2022-06-20 15:02:34 +0300

    some utic sample questions are added

--- Luxoft.GeneralCsTest/UticQuestionSamples.cs
+++ Luxoft.GeneralCsTest/UticQuestionSamples.cs
@@ -1,6 +1,78 @@
-﻿namespace Luxoft.GeneralCsTest;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Luxoft.GeneralCsTest;
 
 public class UticQuestionSamples
 {
-    
+    private readonly ITestOutputHelper _output;
+
+    public UticQuestionSamples(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void SerializeHashTable_Straightforward_Exception()
+    {
+        var table = new HashSet<int>(new[] { 1, 2, 3, });
+        _output.WriteLine(table.ToString());
+        var raw = JsonConvert.SerializeObject(table);
+        _output.WriteLine(raw);
+    }
+
+    [Fact]
+    public void Throw_FromCatch_FinallyIsExecuted()
+    {
+        try
+        {
+            _output.WriteLine("from try before");
+            throw new Exception("test");
+            _output.WriteLine("from try after");
+        }
+        catch (Exception exc)
+        {
+            _output.WriteLine("from catch before");
+            throw;
+            _output.WriteLine("from catch after");
+        }
+        finally
+        {
+            _output.WriteLine("from finally");
+        }
+    }
+
+    [Fact]
+    public void StackOverflowException_IsNotCaught()
+    {
+        try
+        {
+            throw new StackOverflowException("test");
+            _output.WriteLine("from try after");
+        }
+        catch (Exception exc)
+        {
+            _output.WriteLine("from catch before");
+            throw;
+            _output.WriteLine("from catch after");
+        }
+        finally
+        {
+            _output.WriteLine("from finally");
+        }
+    }
+
+    [Fact]
+    public async Task Await_InsideLock_Forbidden()
+    {
+        lock (this)
+        {
+            _ = await File.ReadAllTextAsync("a");
+        }
+    }
 }
--- LuxoftUtic/UjvalsProposal/MainWindow.xaml.cs
+++ LuxoftUtic/UjvalsProposal/MainWindow.xaml.cs
@@ -12,65 +12,25 @@ namespace UjvalsProposal
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    //public partial class MainWindow : Window
-    //{
-    //    private readonly Course course;
-
-    //    public MainWindow()
-    //    {
-    //        InitializeComponent();
-
-    //        var bw = new BackgroundWorker();//older approach
-    //        bw.DoWork += bw_DoWork;
-    //        bw.RunWorkerAsync();
-
-    //        course = new Course();
-    //        course.Name = "The main course";
-    //        course.Students = new List<Student>();
-    //        DataContext = course;
-    //    }
-
-    //    private void bw_DoWork(object? sender, DoWorkEventArgs e)
-    //    {
-    //        for (int i = 0; i < 10; i++)
-    //        {
-    //            var student = new Student()
-    //            {
-    //                Name = $"George the {i} th",
-    //                Age = 20 + i
-    //            };
-    //            Dispatcher.BeginInvoke(() => course.Students.Add(student), DispatcherPriority.DataBind);
-    //            //course.Students.Add(student);
-
-    //            Dispatcher.Invoke(() => tbCount.Text = $"Total Students: {course.Students.Count}",
-    //                DispatcherPriority.DataBind);
-
-    //            Thread.Sleep(TimeSpan.FromSeconds(1));
-    //        }
-    //    }
-
-    //}
-
     public partial class MainWindow : Window
     {
-        public Course Course { get; }
+        private readonly Course course;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            this.Loaded += DoWork;
+            var bw = new BackgroundWorker();//older approach
+            bw.DoWork += bw_DoWork;
+            bw.RunWorkerAsync();
 
-            Course = new Course
-            {
-                Name = "The main course",
-                Students = new List<Student>()
-            };
-
-            DataContext = Course;
+            course = new Course();
+            course.Name = "The main course";
+            course.Students = new List<Student>();
+            DataContext = course;
         }
 
-        private async void DoWork(object sender, RoutedEventArgs e)
+        private void bw_DoWork(object? sender, DoWorkEventArgs e)
         {
             for (int i = 0; i < 10; i++)
             {
@@ -79,13 +39,53 @@ namespace UjvalsProposal
                     Name = $"George the {i} th",
                     Age = 20 + i
                 };
+                Dispatcher.BeginInvoke(() => course.Students.Add(student), DispatcherPriority.DataBind);
+                //course.Students.Add(student);
 
-                Course.Students.Add(student);
+                Dispatcher.Invoke(() => tbCount.Text = $"Total Students: {course.Students.Count}",
+                    DispatcherPriority.DataBind);
 
-                tbCount.Text = $"Total Students: {Course.Students.Count}";
-                
-                await Task.Delay(TimeSpan.FromSeconds(1));
+                Thread.Sleep(TimeSpan.FromSeconds(1));
             }
         }
+
     }
+
+    //public partial class MainWindow : Window
+    //{
+    //    public Course Course { get; }
+
+    //    public MainWindow()
+    //    {
+    //        InitializeComponent();
+
+    //        this.Loaded += DoWork;
+
+    //        Course = new Course
+    //        {
+    //            Name = "The main course",
+    //            Students = new List<Student>()
+    //        };
+
+    //        DataContext = Course;
+    //    }
+
+    //    private async void DoWork(object sender, RoutedEventArgs e)
+    //    {
+    //        for (int i = 0; i < 10; i++)
+    //        {
+    //            var student = new Student()
+    //            {
+    //                Name = $"George the {i} th",
+    //                Age = 20 + i
+    //            };
+
+    //            Course.Students.Add(student);
+
+    //            tbCount.Text = $"Total Students: {Course.Students.Count}";
+
+    //            await Task.Delay(TimeSpan.FromSeconds(1));
+    //        }
+    //    }
+    //}
 }
