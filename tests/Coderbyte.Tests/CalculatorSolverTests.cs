09fcddbdf039be313bed09203d11392352b055b7
Illia Levandovskyi <lis2889@gmail.com>
2024-03-20 17:14:15 +0100

    calculator

---
+++ tests/Coderbyte.Tests/CalculatorSolverTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+
+namespace Coderbyte.Tests;
+
+[Trait("Category", "Unit")]
+public class CalculatorSolverTests
+{
+    [Fact]
+    public void Calculator_OneParenthesis_MatchExpectation()
+    {
+        var input = "6*(4/2)+3*1";
+        var result = CalculatorSolver.Calculator(input);
+        result.Should().Be("15");
+    }
+
+    [Fact]
+    public void Calculator_TwoParenthesis_MatchExpectation()
+    {
+        var input = "(12/2)(4/2)+3*1";
+        var result = CalculatorSolver.Calculator(input);
+        result.Should().Be("15");
+    }
+
+    [Fact]
+    public void Calculator_NestedParenthesis_MatchExpectation()
+    {
+        var input = "((2*5+7-5)/2)(4/2)+3*1";
+        var result = CalculatorSolver.Calculator(input);
+        result.Should().Be("15");
+    }
+
+    [Fact]
+    public void Calculator_NegativeSubEvaluation_MatchExpectation()
+    {
+        var input = "1+((2-3)(5+1))";
+        var result = CalculatorSolver.Calculator(input);
+        result.Should().Be("-5");
+    }
+}
